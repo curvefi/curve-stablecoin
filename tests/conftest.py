4aa8318e537b6c23663faf344f7a721578d00e86
Michael Egorov <michwill@gmail.com>
2022-02-13 14:25:59 +0100

    Approvals after deployment

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,9 +20,13 @@ def borrowed_token(ERC20Mock, accounts):
 
 @pytest.fixture(scope="module", autouse=True)
 def amm(AMM, PriceOracle, collateral_token, borrowed_token, accounts):
-    return AMM.deploy(
+    amm = AMM.deploy(
         collateral_token, borrowed_token,
         100, PRICE * 10**18, 10**16,
         accounts[0],
         PriceOracle, PriceOracle.price.signature,
         {'from': accounts[0]})
+    for acct in accounts[1:6]:
+        collateral_token.approve(amm, 2**256-1, {'from': acct})
+        borrowed_token.approve(amm, 2**256-1, {'from': acct})
+    return amm
