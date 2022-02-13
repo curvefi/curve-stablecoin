66fb92ec96796da609dfae3c60c123ed158a8952
Michael Egorov <michwill@gmail.com>
2022-02-13 16:18:13 +0100

    Deposit test

---
+++ tests/test_deposit_withdraw.py
@@ -0,0 +1,24 @@
+import brownie
+from brownie.test import given, strategy
+
+
+@given(
+        amounts=strategy('uint256[5]', min_value=0, max_value=10**6 * 10**18),
+        ns=strategy('int256[5]', min_value=-20, max_value=20),
+        dns=strategy('uint256[5]', min_value=0, max_value=20),
+)
+def test_deposit_withdraw(amm, amounts, accounts, ns, dns, collateral_token):
+    admin = accounts[0]
+    n0 = amm.active_band()
+    for user, amount, n1, dn in zip(accounts[1:6], amounts, ns, dns):
+        n2 = n1 + dn
+        collateral_token._mint_for_testing(user, amount)
+        if n1 <= n0:
+            with brownie.reverts('Deposits should be below current band'):
+                amm.deposit_range(user, amount, n1, n2, True, {'from': admin})
+        else:
+            if amount // (dn + 1) == 0:
+                with brownie.reverts('Amount too low'):
+                    amm.deposit_range(user, amount, n1, n2, True, {'from': admin})
+            else:
+                amm.deposit_range(user, amount, n1, n2, True, {'from': admin})
