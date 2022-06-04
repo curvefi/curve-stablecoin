a796fe323e57ece85a9bab089e35a416d94b497e
Michael Egorov <michwill@gmail.com>
2022-06-04 15:15:35 +0200

    Unit test for health overflow fixed

--- tests/lendborrow/test_create_repay_stateful.py
+++ tests/lendborrow/test_create_repay_stateful.py
@@ -61,7 +61,7 @@ class StatefulLendBorrow:
         except Exception:
             return  # Probably overflow
 
-        if c_amount**2 >= 2**128:
+        if c_amount >= 2**128:
             with brownie.reverts():
                 self.controller.create_loan(c_amount, amount, n, {'from': user})
             return
@@ -169,6 +169,5 @@ def test_overflow(market_amm, market_controller, collateral_token, stablecoin, a
 
 def test_health_overflow(market_amm, market_controller, collateral_token, stablecoin, accounts, state_machine):
     state = StatefulLendBorrow(market_amm, market_controller, collateral_token, stablecoin, accounts)
-    with brownie.reverts():
-        state.rule_create_loan(amount=256, c_amount=2787635851270792912435800128182537894764544, n=5, user=accounts[0])
-    # state.invariant_health()
+    state.rule_create_loan(amount=256, c_amount=2787635851270792912435800128182537894764544, n=5, user=accounts[0])
+    state.invariant_health()
