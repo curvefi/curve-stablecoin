99599cabbb1f50a71e916d245c680ad1b1a38462
Michael Egorov <michwill@gmail.com>
2022-07-15 12:56:27 +0200

    market

--- boa_tests/test_boa.py
+++ boa_tests/test_boa.py
@@ -6,6 +6,9 @@ from eth.vm.forks.spurious_dragon import computation
 computation.EIP170_CODE_SIZE_LIMIT = 640000  # 640 KB will be enough for everyone
 
 
+PRICE = 3000
+
+
 @pytest.fixture(scope="module", autouse=True)
 def accounts():
     return [boa.env.generate_address() for i in range(10)]
@@ -20,6 +23,12 @@ def admin():
 def stablecoin(admin):
     with boa.env.prank(admin):
         return boa.load('contracts/Stablecoin.vy', 'Curve USD', 'crvUSD')
+
+
+@pytest.fixture(scope="module", autouse=True)
+def collateral_token(admin):
+    with boa.env.prank(admin):
+        return boa.load('contracts/testing/ERC20Mock.vy', "Colalteral", "ETH", 18)
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -40,5 +49,42 @@ def amm_impl(stablecoin, admin):
         return boa.load('contracts/AMM.vy', stablecoin.address)
 
 
-def test_stablecoin(stablecoin, controller_impl):
+@pytest.fixture(scope="module", autouse=True)
+def controller_factory(controller_prefactory, amm_impl, controller_impl, stablecoin, admin):
+    with boa.env.prank(admin):
+        controller_prefactory.set_implementations(controller_impl.address, amm_impl.address)
+        stablecoin.set_minter(controller_prefactory.address, True)
+        stablecoin.set_minter(admin, False)
+    return controller_prefactory
+
+
+@pytest.fixture(scope="module", autouse=True)
+def monetary_policy(admin):
+    with boa.env.prank(admin):
+        policy = boa.load('contracts/mpolicies/ConstantMonetaryPolicy.vy', admin)
+        policy.set_rate(0)
+        return policy
+
+
+@pytest.fixture(scope="module", autouse=True)
+def price_oracle(admin):
+    with boa.env.prank(admin):
+        oracle = boa.load('contracts/testing/DummyPriceOracle.vy', admin, PRICE * 10**18)
+        return oracle
+
+
+@pytest.fixture(scope="module", autouse=True)
+def market(controller_factory, collateral_token, monetary_policy, price_oracle, admin):
+    with boa.env.prank(admin):
+        if controller_factory.n_collaterals() == 0:
+            controller_factory.add_market(
+                collateral_token.address, 100, 10**16, 0,
+                price_oracle.address,
+                monetary_policy.address, 5 * 10**16, 2 * 10**16,
+                10**6 * 10**18)
+        return controller_factory
+
+
+def test_stablecoin(stablecoin, collateral_token, market, amm_impl):
     assert stablecoin.decimals() == 18
+    assert False
