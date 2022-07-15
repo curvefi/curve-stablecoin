0628c635e5bd5dbb96b51e845a857ad1b2b942a7
Michael Egorov <michwill@gmail.com>
2022-07-15 11:38:31 +0200

    More init

--- boa_tests/test_boa.py
+++ boa_tests/test_boa.py
@@ -4,19 +4,37 @@ import pytest
 
 @pytest.fixture(scope="module", autouse=True)
 def accounts():
-    return [i.to_bytes(length=20, byteorder='little') for i in range(10)]
+    return [boa.env.generate_address() for i in range(10)]
 
 
 @pytest.fixture(scope="module", autouse=True)
 def admin():
-    return (1337).to_bytes(length=20, byteorder='big')
+    return boa.env.generate_address()
 
 
 @pytest.fixture(scope="module", autouse=True)
-def stablecoin(accounts, admin):
+def stablecoin(admin):
     with boa.env.prank(admin):
         return boa.load('contracts/Stablecoin.vy', 'Curve USD', 'crvUSD')
 
 
-def test_stablecoin(stablecoin):
+@pytest.fixture(scope="module", autouse=True)
+def controller_prefactory(stablecoin, admin, accounts):
+    with boa.env.prank(admin):
+        return boa.load('contracts/ControllerFactory.vy', stablecoin.address, admin, accounts[0])
+
+
+@pytest.fixture(scope="module", autouse=True)
+def controller_impl(controller_prefactory, admin):
+    with boa.env.prank(admin):
+        return boa.load('contracts/Controller.vy', controller_prefactory.address)
+
+
+@pytest.fixture(scope="module", autouse=True)
+def amm_impl(stablecoin, admin):
+    with boa.env.prank(admin):
+        return boa.load('contracts/AMM.vy', stablecoin.address)
+
+
+def test_stablecoin(stablecoin, controller_impl):
     assert stablecoin.decimals() == 18
