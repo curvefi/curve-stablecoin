521ed47899efb0ca3eb03014b0b2cac525c23c50
Michael Egorov <michwill@gmail.com>
2022-08-25 23:24:12 +0200

    Init the test

--- boa_tests/lendborrow/stabilize/test_price_aggregation.py
+++ boa_tests/lendborrow/stabilize/test_price_aggregation.py
@@ -1,2 +1,15 @@
-def test_price_aggregator(stablecoin, stablecoin_a, stablecoin_b, stableswap_a, stableswap_b):
-    pass
+import boa
+
+
+def test_price_aggregator(stablecoin, stablecoin_a, stablecoin_b, stableswap_a, stableswap_b, admin):
+    with boa.env.prank(admin):
+        stablecoin_a._mint_for_testing(admin, 500000 * 10**6)
+        stablecoin_b._mint_for_testing(admin, 500000 * 10**18)
+
+        stablecoin_a.approve(stableswap_a.address, 2**256-1)
+        stablecoin.approve(stableswap_a.address, 2**256-1)
+        stablecoin_b.approve(stableswap_b.address, 2**256-1)
+        stablecoin.approve(stableswap_b.address, 2**256-1)
+
+        stableswap_a.add_liquidity([500000 * 10**6, 500000 * 10**18], 0)
+        stableswap_b.add_liquidity([500000 * 10**18, 500000 * 10**18], 0)
