9470e9287e029d4f3031f094c367a35e1dd0a63d
Michael Egorov <michwill@gmail.com>
2022-08-26 22:37:09 +0200

    Price aggregator fixture

--- boa_tests/lendborrow/stabilize/test_price_aggregation.py
+++ boa_tests/lendborrow/stabilize/test_price_aggregation.py
@@ -1,7 +1,7 @@
 import boa
 
 
-def test_price_aggregator(stablecoin, stablecoin_a, stablecoin_b, stableswap_a, stableswap_b, admin):
+def test_price_aggregator(stablecoin, stablecoin_a, stablecoin_b, stableswap_a, stableswap_b, price_aggregator, admin):
     with boa.env.prank(admin):
         stablecoin_a._mint_for_testing(admin, 500000 * 10**6)
         stablecoin_b._mint_for_testing(admin, 500000 * 10**18)
