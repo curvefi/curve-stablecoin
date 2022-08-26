0e3c72b8978e282b4f76c5eecd02ea86963ae76e
Michael Egorov <michwill@gmail.com>
2022-08-26 19:33:12 +0200

    Switching to exp() instead of 2**

--- model/avg.py
+++ model/avg.py
@@ -8,13 +8,14 @@ def calc_price(D, p, sigma=0.001):
     p_pre = (D * p).sum() / D.sum()
     e = (p - p_pre)**2 / sigma**2
     e -= e.min()
-    w = D * 2**(-e)
+    w = D * np.exp(-e)
     return (w * p).sum() / w.sum()
 
 
 if __name__ == '__main__':
-    D = [100, 100, 100]
+    N = 4
+    D = [100] * N
     p = np.linspace(0.9, 1.1, 1000)
-    p_new = [calc_price(D, [_p, 1, 1]) for _p in p]
+    p_new = [calc_price(D, [_p] + [1] * (N - 1)) for _p in p]
     pylab.plot(p, p_new)
     pylab.show()
