bec0ee5b629c4b224b2300e751ecb82dd97521cb
Michael Egorov <michwill@gmail.com>
2022-08-28 00:28:01 +0200

    Revert unsafe test ops in stablize module

--- boa_tests/lendborrow/stabilize/test_price_aggregation.py
+++ boa_tests/lendborrow/stabilize/test_price_aggregation.py
@@ -5,18 +5,19 @@ import boa
 
 @pytest.fixture(scope="module")
 def agg(stablecoin, stablecoin_a, stablecoin_b, stableswap_a, stableswap_b, price_aggregator, admin):
-    with boa.env.prank(admin):
-        stablecoin_a._mint_for_testing(admin, 500000 * 10**6)
-        stablecoin_b._mint_for_testing(admin, 500000 * 10**18)
+    with boa.env.anchor():
+        with boa.env.prank(admin):
+            stablecoin_a._mint_for_testing(admin, 500000 * 10**6)
+            stablecoin_b._mint_for_testing(admin, 500000 * 10**18)
 
-        stablecoin_a.approve(stableswap_a.address, 2**256-1)
-        stablecoin.approve(stableswap_a.address, 2**256-1)
-        stablecoin_b.approve(stableswap_b.address, 2**256-1)
-        stablecoin.approve(stableswap_b.address, 2**256-1)
+            stablecoin_a.approve(stableswap_a.address, 2**256-1)
+            stablecoin.approve(stableswap_a.address, 2**256-1)
+            stablecoin_b.approve(stableswap_b.address, 2**256-1)
+            stablecoin.approve(stableswap_b.address, 2**256-1)
 
-        stableswap_a.add_liquidity([500000 * 10**6, 500000 * 10**18], 0)
-        stableswap_b.add_liquidity([500000 * 10**18, 500000 * 10**18], 0)
-    return price_aggregator
+            stableswap_a.add_liquidity([500000 * 10**6, 500000 * 10**18], 0)
+            stableswap_b.add_liquidity([500000 * 10**18, 500000 * 10**18], 0)
+        yield price_aggregator
 
 
 def test_price_aggregator(stablecoin_a, agg):
