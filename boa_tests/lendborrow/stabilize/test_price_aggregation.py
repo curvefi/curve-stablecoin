b13d9bee40ce405047fd083eded0f2b5a16eae07
Michael Egorov <michwill@gmail.com>
2022-08-28 00:18:28 +0200

    Easiest test for the aggregator

--- boa_tests/lendborrow/stabilize/test_price_aggregation.py
+++ boa_tests/lendborrow/stabilize/test_price_aggregation.py
@@ -1,7 +1,10 @@
+import pytest
+from ...conftest import approx
 import boa
 
 
-def test_price_aggregator(stablecoin, stablecoin_a, stablecoin_b, stableswap_a, stableswap_b, price_aggregator, admin):
+@pytest.fixture(scope="module")
+def agg(stablecoin, stablecoin_a, stablecoin_b, stableswap_a, stableswap_b, price_aggregator, admin):
     with boa.env.prank(admin):
         stablecoin_a._mint_for_testing(admin, 500000 * 10**6)
         stablecoin_b._mint_for_testing(admin, 500000 * 10**18)
@@ -13,3 +16,8 @@ def test_price_aggregator(stablecoin, stablecoin_a, stablecoin_b, stableswap_a, 
 
         stableswap_a.add_liquidity([500000 * 10**6, 500000 * 10**18], 0)
         stableswap_b.add_liquidity([500000 * 10**18, 500000 * 10**18], 0)
+    return price_aggregator
+
+
+def test_price_aggregator(stablecoin_a, agg):
+    assert approx(agg.price(), 10**18, 1e-6)
