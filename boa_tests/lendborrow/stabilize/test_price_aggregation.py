c3c86cbbe875967223b24995d969c7cf7b6adf3e
Michael Egorov <michwill@gmail.com>
2022-08-30 16:38:35 +0200

    Test price aggregation properly

--- boa_tests/lendborrow/stabilize/test_price_aggregation.py
+++ boa_tests/lendborrow/stabilize/test_price_aggregation.py
@@ -60,16 +60,28 @@ def test_price_aggregator(stableswap_a, stableswap_b, stablecoin_a, agg, admin):
             assert approx(agg.price(), (p_o + 10**18) / 2, 1e-3)
 
 
-def test_crypto_agg(dummy_tricrypto, crypto_agg, admin):
-    assert dummy_tricrypto.price_oracle(0) == 3000 * 10**18
-    assert crypto_agg.price() == 3000 * 10**18
+def test_crypto_agg(dummy_tricrypto, crypto_agg, stableswap_a, stablecoin_a, admin):
+    with boa.env.anchor():
+        with boa.env.prank(admin):
+            assert dummy_tricrypto.price_oracle(0) == 3000 * 10**18
+            assert crypto_agg.price() == 3000 * 10**18
 
-    with boa.env.prank(admin):
-        dummy_tricrypto.set_price(0, 1000 * 10**18)
-    assert dummy_tricrypto.price_oracle(0) == 1000 * 10**18
-    assert crypto_agg.price() == 3000 * 10**18
-    assert crypto_agg.raw_price() == 1000 * 10**18
+            with boa.env.prank(admin):
+                dummy_tricrypto.set_price(0, 1000 * 10**18)
+            assert dummy_tricrypto.price_oracle(0) == 1000 * 10**18
+            assert crypto_agg.price() == 3000 * 10**18
+            assert crypto_agg.raw_price() == 1000 * 10**18
 
-    time_travel(200_000)
+            time_travel(200_000)
 
-    assert approx(crypto_agg.price(), 1000 * 10**18, 1e-10)
+            p = crypto_agg.price()
+            assert approx(p, 1000 * 10**18, 1e-10)
+
+            amount = 300_000 * 10**6
+            stablecoin_a._mint_for_testing(admin, amount)
+            stableswap_a.exchange(0, 1, amount, 0)
+
+            time_travel(200_000)
+            p = stableswap_a.price_oracle()
+            assert p > 10**18 * 1.01
+            assert crypto_agg.price() > p * 1.01
