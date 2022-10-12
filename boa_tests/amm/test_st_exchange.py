7d58081b461a9fb3f88c10862d7a73e228f33207
Michael Egorov <michwill@gmail.com>
2022-10-12 16:11:40 +0200

    st_exchange ported

--- boa_tests/amm/test_st_exchange.py
+++ boa_tests/amm/test_st_exchange.py
@@ -1,27 +1,32 @@
-from brownie.test import strategy
+import boa
+from hypothesis import settings
+from hypothesis import strategies as st
+from hypothesis.stateful import RuleBasedStateMachine, run_state_machine_as_test, rule, invariant, initialize
+from datetime import timedelta
 
 
-class StatefulExchange:
-    amounts = strategy('uint256[5]', min_value=0, max_value=10**6 * 10**18)
-    ns = strategy('int256[5]', min_value=1, max_value=20)
-    dns = strategy('uint256[5]', min_value=0, max_value=20)
-    amount = strategy('uint256', max_value=10**9 * 10**6)
-    pump = strategy('bool')
-    user_id = strategy('uint256', max_value=4)
+class StatefulExchange(RuleBasedStateMachine):
+    amounts = st.lists(st.integers(min_value=0, max_value=10**6 * 10**18), min_size=5, max_size=5)
+    ns = st.lists(st.integers(min_value=1, max_value=20), min_size=5, max_size=5)
+    dns = st.lists(st.integers(min_value=0, max_value=20), min_size=5, max_size=5)
+    amount = st.integers(min_value=0, max_value=10**9 * 10**6)
+    pump = st.booleans()
+    user_id = st.integers(min_value=0, max_value=4)
 
-    def __init__(self, amm, collateral_token, borrowed_token, accounts):
-        self.amm = amm
-        self.collateral_token = collateral_token
-        self.borrowed_token = borrowed_token
-        self.accounts = accounts[1:6]
-        self.admin = accounts[0]
+    def __init__(self):
+        super().__init__()
+        self.anchor = boa.env.anchor()
+        self.anchor.__enter__()
+        self.total_deposited = 0
 
-    def initialize(self, amounts, ns, dns):
+    @initialize(amounts=amounts, ns=ns, dns=dns)
+    def initializer(self, amounts, ns, dns):
         for user, amount, n1, dn in zip(self.accounts, amounts, ns, dns):
             n2 = n1 + dn
             self.collateral_token._mint_for_testing(user, amount)
             try:
-                self.amm.deposit_range(user, amount, n1, n2, True, {'from': self.admin})
+                with boa.env.prank(self.admin):
+                    self.amm.deposit_range(user, amount, n1, n2, True)
             except Exception as e:
                 if 'Amount too low' in str(e):
                     assert amount // (dn + 1) <= 100
@@ -29,7 +34,8 @@ class StatefulExchange:
                     raise
         self.total_deposited = sum(self.amm.bands_y(n) for n in range(42))
 
-    def rule_exchange(self, amount, pump, user_id):
+    @rule(amount=amount, pump=pump, user_id=user_id)
+    def exchange(self, amount, pump, user_id):
         u = self.accounts[user_id]
         if pump:
             i = 0
@@ -42,15 +48,18 @@ class StatefulExchange:
         u_amount = in_token.balanceOf(u)
         if amount > u_amount:
             in_token._mint_for_testing(u, amount - u_amount)
-        self.amm.exchange(i, j, amount, 0, {'from': u})
+        with boa.env.prank(u):
+            self.amm.exchange(i, j, amount, 0)
 
-    def invariant_amm_solvent(self):
+    @invariant()
+    def amm_solvent(self):
         X = sum(self.amm.bands_x(n) for n in range(42))
         Y = sum(self.amm.bands_y(n) for n in range(42))
         assert self.borrowed_token.balanceOf(self.amm) * 10**(18 - 6) >= X
         assert self.collateral_token.balanceOf(self.amm) >= Y
 
-    def invariant_dy_back(self):
+    @invariant()
+    def dy_back(self):
         n = self.amm.active_band()
         to_swap = self.total_deposited * 10
         left_in_amm = sum(self.amm.bands_y(n) for n in range(42))
@@ -59,7 +68,7 @@ class StatefulExchange:
             assert dx >= self.total_deposited - left_in_amm  # With fees, AMM will have more
 
     def teardown(self):
-        u = self.accounts[1]
+        u = self.accounts[0]
         # Trade back and do the check
         n = self.amm.active_band()
         to_swap = self.total_deposited * 10
@@ -67,10 +76,16 @@ class StatefulExchange:
             _, dy = self.amm.get_dxdy(1, 0, to_swap)
             if dy > 0:
                 self.collateral_token._mint_for_testing(u, to_swap)
-                self.amm.exchange(1, 0, to_swap, 0, {'from': u})
+                with boa.env.prank(u):
+                    self.amm.exchange(1, 0, to_swap, 0)
                 left_in_amm = sum(self.amm.bands_y(n) for n in range(42))
                 assert left_in_amm >= self.total_deposited
+        self.anchor.__exit__(None, None, None)
 
 
-def test_exchange(accounts, amm, collateral_token, borrowed_token, state_machine):
-    state_machine(StatefulExchange, amm, collateral_token, borrowed_token, accounts)
+def test_exchange(admin, accounts, amm, collateral_token, borrowed_token):
+    StatefulExchange.TestCase.settings = settings(deadline=timedelta(seconds=1000))
+    accounts = accounts[:5]
+    for k, v in locals().items():
+        setattr(StatefulExchange, k, v)
+    run_state_machine_as_test(StatefulExchange)
