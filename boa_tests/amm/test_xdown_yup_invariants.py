725ce8e0dd3d7c5da5fae4d11eb04d20a2ca2991
Michael Egorov <michwill@gmail.com>
2022-10-12 13:57:27 +0200

    xdown_yup_invariants ported to boa

--- boa_tests/amm/test_xdown_yup_invariants.py
+++ boa_tests/amm/test_xdown_yup_invariants.py
@@ -1,15 +1,138 @@
-from hypothesis import given, strategies, settings
+from hypothesis import given, settings
+from hypothesis import strategies as st
 import boa
 from datetime import timedelta
 from ..conftest import approx
+"""
+Test that get_x_down and get_y_up don't change:
+* if we do trades at constant p_o (immediate trades)
+* or if we do adiabatic trade keeping p = p_o (adiabatic_trades)
+"""
 
 
 @given(
-    p_o_1=strategies.integers(min_value=2000 * 10**18, max_value=4000 * 10**18),
-    p_o_2=strategies.integers(min_value=2000 * 10**18, max_value=4000 * 10**18),
-    n1=strategies.integers(min_value=1, max_value=30),
-    dn=strategies.integers(min_value=0, max_value=30),
-    deposit_amount=strategies.integers(min_value=10**18, max_value=10**25),
+    p_o=st.integers(min_value=1000 * 10**18, max_value=10000 * 10**18),
+    n1=st.integers(min_value=1, max_value=30),
+    dn=st.integers(min_value=0, max_value=30),
+    deposit_amount=st.integers(min_value=10**9, max_value=10**25),
+    f_pump=st.floats(min_value=0, max_value=10),
+    f_trade=st.floats(min_value=0, max_value=10),
+    is_pump=st.booleans()
+)
+@settings(deadline=timedelta(seconds=1000))
+def test_immediate(amm, price_oracle, collateral_token, borrowed_token, accounts, admin,
+                   p_o, n1, dn, deposit_amount, f_pump, f_trade, is_pump):
+    user = accounts[0]
+    with boa.env.anchor():
+        with boa.env.prank(admin):
+            price_oracle.set_price(p_o)
+            amm.set_fee(0)
+            collateral_token._mint_for_testing(user, deposit_amount)
+            amm.deposit_range(user, deposit_amount, n1, n1+dn, True)
+        pump_amount = int(p_o * deposit_amount / 10**18 * f_pump / 10**12)
+        with boa.env.prank(user):
+            borrowed_token._mint_for_testing(user, pump_amount)
+            amm.exchange(0, 1, pump_amount, 0)
+
+        x0 = amm.get_x_down(user)
+        y0 = amm.get_y_up(user)
+
+        if is_pump:
+            trade_amount = int(p_o * deposit_amount / 10**18 * f_trade / 10**12)
+            with boa.env.prank(user):
+                borrowed_token._mint_for_testing(user, trade_amount)
+            i = 0
+            j = 1
+        else:
+            trade_amount = int(deposit_amount * f_trade)
+            with boa.env.prank(user):
+                collateral_token._mint_for_testing(user, trade_amount)
+            i = 1
+            j = 0
+
+        with boa.env.prank(user):
+            amm.exchange(i, j, trade_amount, 0)
+
+        x1 = amm.get_x_down(user)
+        y1 = amm.get_y_up(user)
+
+        assert approx(x0, x1, 1e-9, 100)
+        assert approx(y0, y1, 1e-9, 100)
+
+
+def test_immediate_above_p0(amm, price_oracle, collateral_token, borrowed_token, accounts, admin):
+    deposit_amount = 5805319702344997833315303
+    user = accounts[0]
+
+    with boa.env.anchor():
+        with boa.env.prank(admin):
+            amm.set_fee(0)
+            collateral_token._mint_for_testing(user, deposit_amount)
+            amm.deposit_range(user, deposit_amount, 6, 6, True)
+
+        pump_amount = 3000 * deposit_amount * 147 // 10**18 // 10**12
+        with boa.env.prank(user):
+            borrowed_token._mint_for_testing(user, pump_amount)
+            amm.exchange(0, 1, pump_amount, 0)
+
+        x0 = amm.get_x_down(user)
+        y0 = amm.get_y_up(user)
+
+        trade_amount = deposit_amount * 52469 // 10**18
+        collateral_token._mint_for_testing(user, trade_amount)
+
+        with boa.env.prank(user):
+            amm.exchange(1, 0, trade_amount, 0)
+
+        x1 = amm.get_x_down(user)
+        y1 = amm.get_y_up(user)
+
+        assert x0 > 0
+        assert x1 > 0
+        assert approx(y0, deposit_amount, 1e-9)
+        assert approx(x0, x1, 1e-9)
+        assert approx(y0, y1, 1e-9)
+
+
+def test_immediate_in_band(amm, price_oracle, collateral_token, borrowed_token, accounts, admin):
+    deposit_amount = 835969548449222546344625
+
+    with boa.env.anchor():
+        user = accounts[0]
+        with boa.env.prank(admin):
+            amm.set_fee(0)
+            collateral_token._mint_for_testing(user, deposit_amount)
+            amm.deposit_range(user, deposit_amount, 4, 4, True)
+
+        pump_amount = 137
+        with boa.env.prank(user):
+            borrowed_token._mint_for_testing(user, pump_amount)
+            amm.exchange(0, 1, pump_amount, 0)
+
+        x0 = amm.get_x_down(user)
+        y0 = amm.get_y_up(user)
+
+        trade_amount = 2690425910633510  # 181406004646580
+        with boa.env.prank(user):
+            borrowed_token._mint_for_testing(user, trade_amount)
+            amm.exchange(0, 1, trade_amount, 0)
+
+        x1 = amm.get_x_down(user)
+        y1 = amm.get_y_up(user)
+
+        assert x0 > 0
+        assert x1 > 0
+        assert approx(y0, deposit_amount, 1e-9)
+        assert approx(x0, x1, 1e-9)
+        assert approx(y0, y1, 1e-9)
+
+
+@given(
+    p_o_1=st.integers(min_value=2000 * 10**18, max_value=4000 * 10**18),
+    p_o_2=st.integers(min_value=2000 * 10**18, max_value=4000 * 10**18),
+    n1=st.integers(min_value=1, max_value=30),
+    dn=st.integers(min_value=0, max_value=30),
+    deposit_amount=st.integers(min_value=10**18, max_value=10**25),
 )
 @settings(max_examples=100, deadline=timedelta(seconds=1000))
 def test_adiabatic(amm, price_oracle, collateral_token, borrowed_token, accounts, admin,
