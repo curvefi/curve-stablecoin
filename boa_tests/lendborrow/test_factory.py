651f61cca4de3b7d6977f801aef7761300014590
Michael Egorov <michwill@gmail.com>
2022-10-13 08:18:30 +0200

    test_factory ported to boa -> lower() added to prevent an odd boa error

---
+++ boa_tests/lendborrow/test_factory.py
@@ -0,0 +1,56 @@
+import boa
+
+
+def test_stablecoin_admin(controller_factory, stablecoin, accounts):
+    with boa.env.anchor():
+        assert stablecoin.minters(controller_factory)
+        assert not stablecoin.minters(accounts[0])
+        with boa.reverts():
+            with boa.env.prank(accounts[0]):
+                assert stablecoin.set_minter(accounts[1], True)
+        with boa.env.prank(controller_factory.address):
+            stablecoin.set_minter(accounts[1], True)
+            stablecoin.set_minter(accounts[1], False)
+
+
+def test_stablecoin(controller_factory, stablecoin):
+    assert controller_factory.stablecoin() == stablecoin.address
+
+
+def test_impl(controller_factory, controller_impl, amm_impl):
+    assert controller_factory.controller_implementation() == controller_impl.address
+    assert controller_factory.amm_implementation() == amm_impl.address
+
+
+def test_add_market(controller_factory, collateral_token, price_oracle, monetary_policy, admin,
+                    controller_interface, amm_interface):
+    # token: address, A: uint256, fee: uint256, admin_fee: uint256,
+    # _price_oracle_contract: address,
+    # monetary_policy: address, loan_discount: uint256, liquidation_discount: uint256,
+    # debt_ceiling: uint256) -> address[2]:
+    with boa.env.anchor():
+        with boa.env.prank(admin):
+            controller_factory.add_market(
+                collateral_token.address, 100, 10**16, 0,
+                price_oracle.address,
+                monetary_policy.address, 5 * 10**16, 2 * 10**16,
+                10**8 * 10**18)
+
+            assert controller_factory.n_collaterals() == 1
+            assert controller_factory.collaterals(0).lower() == collateral_token.address.lower()
+
+            controller = controller_interface.at(controller_factory.get_controller(collateral_token.address))
+            amm = amm_interface.at(controller_factory.get_amm(collateral_token.address))
+
+            assert controller.factory().lower() == controller_factory.address.lower()
+            assert controller.collateral_token().lower() == collateral_token.address.lower()
+            assert controller.amm().lower() == amm.address.lower()  # <- boa fails without lower here?!? XXX
+            assert controller.monetary_policy().lower() == monetary_policy.address.lower()
+            assert controller.liquidation_discount() == 2 * 10**16
+            assert controller.loan_discount()  == 5 * 10**16
+            assert controller_factory.debt_ceiling(controller) == 10**8 * 10**18
+
+            assert amm.admin().lower() == controller.address.lower()
+            assert amm.A() == 100
+            assert amm.price_oracle_contract().lower() == price_oracle.address.lower()
+            assert amm.collateral_token().lower() == collateral_token.address.lower()
