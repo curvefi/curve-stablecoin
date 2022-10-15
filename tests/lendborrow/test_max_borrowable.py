b492ae137e37d4c180e6302f54532a77f37882da
Michael Egorov <michwill@gmail.com>
2022-10-15 08:53:00 +0200

    Test for min_collateral (fail)

--- tests/lendborrow/test_max_borrowable.py
+++ tests/lendborrow/test_max_borrowable.py
@@ -12,8 +12,17 @@ from ..conftest import approx
 def test_max_borrowable(market_controller, collateral_amount, n):
     max_borrowable = market_controller.max_borrowable(collateral_amount, n)
     with boa.reverts():
-        market_controller.calculate_debt_n1(collateral_amount, int(max_borrowable * 1.001), n)
+        market_controller.calculate_debt_n1(collateral_amount, int(max_borrowable * 1.001) + 1, n)
     market_controller.calculate_debt_n1(collateral_amount, max_borrowable, n)
 
     min_collateral = market_controller.min_collateral(max_borrowable, n)
     assert approx(min_collateral, collateral_amount, 1e-6)
+
+
+@given(
+    debt_amount=st.integers(min_value=10**9, max_value=10**20),
+    n=st.integers(min_value=5, max_value=50))
+@settings(max_examples=100, deadline=timedelta(seconds=1000))
+def test_min_collateral(market_controller, debt_amount, n):
+    min_collateral = market_controller.min_collateral(debt_amount, n)
+    market_controller.calculate_debt_n1(min_collateral, debt_amount, n)
