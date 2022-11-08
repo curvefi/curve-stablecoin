2ea508e5798e2dd8e3964f8e553aac9543c06d38
Edward Amor <edward.amor3@gmail.com>
2022-11-08 02:16:16 -0500

    test: stablecoin.permit

--- tests/stablecoin/test_approve.py
+++ tests/stablecoin/test_approve.py
@@ -1,6 +1,36 @@
-from brownie import ZERO_ADDRESS
+from brownie import ZERO_ADDRESS, accounts
+import brownie
+
+from eth_account._utils.structured_data.hashing import hash_domain, hash_message
+from copy import deepcopy
+from eth_account.messages import SignableMessage
+from eth_account import Account as EthAccount
+
+
+
 
 AMOUNT = 10**21
+
+PERMIT_STRUCT = {
+    "types": {
+        "EIP712Domain": [
+            {"name": "name", "type": "string"},
+            {"name": "version", "type": "string"},
+            {"name": "chainId", "type": "uint256"},
+            {"name": "verifyingContract", "type": "address"},
+            {"name": "salt", "type": "bytes32"}
+        ],
+        "Permit": [
+            {"name": "owner", "type": "address"},
+            {"name": "spender", "type": "address"},
+            {"name": "value", "type": "uint256"},
+            {"name": "nonce", "type": "uint256"},
+            {"name": "deadline", "type": "uint256"},
+        ],
+    },
+    "primaryType": "Permit",
+}
+
 
 
 def test_approve(alice, bob, stablecoin):
@@ -58,3 +88,51 @@ def test_decreaseAllowance_does_not_underflow(alice, bob, stablecoin):
 
     assert "Approval" not in tx.events
     assert tx.return_value is True
+
+def test_permit_success(stablecoin, chain):
+    alice = accounts.add("0x416b8a7d9290502f5661da81f0cf43893e3d19cb9aea3c426cfb36e8186e9c09")
+
+    struct = deepcopy(PERMIT_STRUCT)
+    struct["domain"] = dict(
+        name=stablecoin.name(),
+        version=stablecoin.version(),
+        chainId=chain.id,
+        verifyingContract=stablecoin.address,
+        salt=stablecoin.salt(),
+    )
+    deadline = chain.time() + 600
+    struct["message"] = dict(
+        owner=alice.address,
+        spender=alice.address,
+        value=2**256 -1,
+        nonce=stablecoin.nonces(alice),
+        deadline=deadline,
+    )
+    signable_message = SignableMessage(
+        b"\x01",
+        hash_domain(struct),
+        hash_message(struct)
+    )
+    sig = EthAccount.sign_message(signable_message, alice.private_key)
+    
+    tx = stablecoin.permit(alice, alice, 2**256 -1, deadline, sig.v, sig.r, sig.s)
+
+    assert stablecoin.nonces(alice) == 1
+    assert stablecoin.allowance(alice, alice) == 2**256 -1
+    assert tx.events["Approval"].values() == [alice, alice, 2**256 -1]
+    assert tx.return_value is True
+
+
+def test_permit_reverts_owner_is_invalid(bob, chain, stablecoin):
+    with brownie.reverts():
+        stablecoin.permit(ZERO_ADDRESS, bob, 2**256 - 1, chain.time() + 600, 27, b"\x00" * 32, b"\x00" * 32, {"from": bob})
+
+
+def test_permit_reverts_deadline_is_invalid(bob, chain, stablecoin):
+    with brownie.reverts():
+        stablecoin.permit(bob, bob, 2**256 - 1, chain.time() - 600, 27, b"\x00" * 32, b"\x00" * 32, {"from": bob})
+
+
+def test_permit_reverts_signature_is_invalid(bob, chain, stablecoin):
+    with brownie.reverts():
+        stablecoin.permit(bob, bob, 2**256 - 1, chain.time() + 600, 27, b"\x00" * 32, b"\x00" * 32, {"from": bob})
