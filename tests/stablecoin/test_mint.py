37e20f6ee8b930f5a9c8fbe324a9581760bf1491
Edward Amor <edward.amor3@gmail.com>
2022-11-08 01:02:24 -0500

    test: stablecoin.set_minter

--- tests/stablecoin/test_mint.py
+++ tests/stablecoin/test_mint.py
@@ -22,3 +22,15 @@ def test_mint_reverts_receiver_is_invalid(alice, stablecoin, idx):
     receiver = [ZERO_ADDRESS, stablecoin][idx]
     with brownie.reverts():
         stablecoin.mint(receiver, 10**18, {"from": alice})
+
+
+def test_set_minter(alice, bob, stablecoin):
+    tx = stablecoin.set_minter(bob, {"from": alice})
+
+    assert stablecoin.minter() == bob
+    assert tx.events["SetMinter"].values() == [bob]
+
+
+def test_set_minter_reverts_caller_is_invalid(bob, stablecoin):
+    with brownie.reverts():
+        stablecoin.set_minter(bob, {"from": bob})
