4e8196c5c76048e05c025e31bfd089546e388df4
Edward Amor <edward.amor3@gmail.com>
2022-11-08 01:47:46 -0500

    test: increase/decrease Allowance methods

--- tests/stablecoin/test_approve.py
+++ tests/stablecoin/test_approve.py
@@ -18,3 +18,43 @@ def test_nonzero_to_nonzero_approval(alice, bob, stablecoin):
     assert stablecoin.allowance(alice, bob) == AMOUNT
     assert tx.events["Approval"].values() == [alice, bob, AMOUNT]
     assert tx.return_value is True
+
+
+def test_increaseAllowance(alice, bob, stablecoin):
+    tx = stablecoin.increaseAllowance(bob, AMOUNT, {"from": alice})
+
+    assert stablecoin.allowance(alice, bob) == AMOUNT
+    assert tx.events["Approval"].values() == [alice, bob, AMOUNT]
+    assert tx.return_value is True
+
+
+def test_increaseAllowance_does_not_overflow(alice, bob, stablecoin):
+    for _ in range(10):
+        stablecoin.increaseAllowance(bob, 2 ** 255 - 1, {"from": alice})
+    
+    assert stablecoin.allowance(alice, bob) == 2**256 -1
+    
+    tx = stablecoin.increaseAllowance(bob, 10, {"from": alice})
+    assert "Approval" not in tx.events
+    assert tx.return_value is True
+
+
+def test_decreaseAllowance(alice, bob, stablecoin):
+    stablecoin.approve(bob, AMOUNT, {"from": alice})
+    tx = stablecoin.decreaseAllowance(bob, AMOUNT, {"from": alice})
+
+    assert stablecoin.allowance(alice, bob) == 0
+    assert tx.events["Approval"].values() == [alice, bob, 0]
+    assert tx.return_value is True
+
+
+def test_decreaseAllowance_does_not_underflow(alice, bob, stablecoin):
+    for _ in range(10):
+        stablecoin.decreaseAllowance(bob, 10, {"from": alice})
+    
+    assert stablecoin.allowance(alice, bob) == 0
+
+    tx = stablecoin.decreaseAllowance(bob, 10, {"from": alice})
+
+    assert "Approval" not in tx.events
+    assert tx.return_value is True
