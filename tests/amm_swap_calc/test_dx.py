69273782bbb6dec19223fabac000d45d061aa31c
macket <macket24@proton.me>
2022-12-24 18:13:43 +0400

    test: test_dydx both sides

--- tests/amm_swap_calc/test_dx.py
+++ tests/amm_swap_calc/test_dx.py
@@ -22,25 +22,42 @@ def test_dydx_limits(amm, swap_calc, amounts, accounts, ns, dns, collateral_toke
     dy, dx = swap_calc.get_dydx(amm, 1, 0, 0)
     assert dx == dy == 0
 
-    dx_precision = 10**(max(borrowed_token.decimals() - 14, 0))  # 18 - 10_000, 17 - 1000, 16 - 100, 15 - 10, < 14 - 1
-    print(dx_precision)
+    collateral_deciamls = collateral_token.decimals()
+    borrowed_deciamls = borrowed_token.decimals()
+    dx_precision = 10**(max(borrowed_deciamls - 14, 0))  # 18 - 10_000, 17 - 1000, 16 - 100, 15 - 10, < 14 - 1
 
     # Small swap
-    dx1, dy1 = amm.get_dxdy(0, 1, 10**16)
-    dy2, dx2 = swap_calc.get_dydx(amm, 0, 1, dy1)
+    dy1, dx1 = swap_calc.get_dydx(amm, 0, 1, 10**(collateral_deciamls - 2))
+    dx2, dy2 = amm.get_dxdy(0, 1, dx1)
+    assert dx1 == dx2
     assert dy1 == dy2
-    assert abs(dx1 - dx2) <= dx_precision
-    dy, dx = swap_calc.get_dydx(amm, 1, 0, 10**16)  # No liquidity
-    assert dy == 0
-    assert dx == 0  # Rounded down
+
+    dx1, dy1 = amm.get_dxdy(0, 1, 10**(borrowed_deciamls - 2))
+    dy2, dx2 = swap_calc.get_dydx(amm, 0, 1, dy1)
+    dx3, dy3 = amm.get_dxdy(0, 1, dx2)
+    assert abs(dx1 - dx2) < dx_precision
+    assert dx2 == dx3
+    assert dy1 == dy2 == dy3
+
+    dy, dx = swap_calc.get_dydx(amm, 1, 0, 10**(collateral_deciamls - 2))  # No liquidity
+    assert dx == 0
+    assert dy == 0  # Rounded down
 
     # Huge swap
-    dy1, dx1 = swap_calc.get_dydx(amm, 0, 1, 10**12 * 10**18)
+    dy1, dx1 = swap_calc.get_dydx(amm, 0, 1, 10**12 * 10**collateral_deciamls)
     dx2, dy2 = amm.get_dxdy(0, 1, dx1)
-    assert dy1 < 10**12 * 10**18               # Less than all is desired
-    assert abs(dy1 - sum(amounts)) <= 1000     # but everything is bought
+    assert dy1 < 10**12 * 10**collateral_deciamls      # Less than all is desired
+    assert abs(dy1 - sum(amounts)) <= 1000                   # but everything is bought
+    assert dx1 == dx2
     assert dy1 == dy2
-    assert abs(dx1 - dx2) <= dx_precision
-    dy, dx = swap_calc.get_dydx(amm, 1, 0, 10**12 * 10**18)  # No liquidity
-    assert dy == 0
-    assert dx == 0  # Rounded down
+
+    dx1, dy1 = amm.get_dxdy(0, 1, 10**12 * 10**borrowed_deciamls)
+    dy2, dx2 = swap_calc.get_dydx(amm, 0, 1, dy1)
+    assert dx1 < 10**12 * 10**borrowed_deciamls        # Less than all is spent
+    assert abs(dy1 - sum(amounts)) <= 1000                   # but everything is bought
+    assert dx1 == dx2
+    assert dy1 == dy2
+
+    dy, dx = swap_calc.get_dydx(amm, 1, 0, 10**12 * 10**collateral_deciamls)  # No liquidity
+    assert dx == 0
+    assert dy == 0  # Rounded down
