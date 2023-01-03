cda13c64debbed8ffea9229e4fd205303a2efb16
macket <macket24@proton.me>
2023-01-03 20:47:47 +0400

    test: slippage revert

--- tests/exchange_dy_zap/test_exchange_dy.py
+++ tests/exchange_dy_zap/test_exchange_dy.py
@@ -104,7 +104,9 @@ def test_exchange_dy_down_up(amm, exchange_dy_zap, amounts, accounts, ns, dns, a
     assert approx(dx, dx2, 1e-6)
     borrowed_token._mint_for_testing(u, dx2)
     with boa.env.prank(u):
-        exchange_dy_zap.exchange_dy(amm, 0, 1, dy2, 0)  # crvUSD --> ETH
+        with boa.reverts("Slippage"):
+            exchange_dy_zap.exchange_dy(amm, 0, 1, dy2, dx2 - 1)  # crvUSD --> ETH
+        exchange_dy_zap.exchange_dy(amm, 0, 1, dy2, dx2)  # crvUSD --> ETH
     assert borrowed_token.balanceOf(u) == 0
     assert abs(collateral_token.balanceOf(u) - dy2) <= collateral_precision
 
@@ -124,32 +126,10 @@ def test_exchange_dy_down_up(amm, exchange_dy_zap, amounts, accounts, ns, dns, a
     dy_measured = borrowed_token.balanceOf(u)
     dx_measured = collateral_token.balanceOf(u)
     with boa.env.prank(u):
-        exchange_dy_zap.exchange_dy(amm, 1, 0, out_amount, 0)  # ETH --> crvUSD
+        with boa.reverts("Slippage"):
+            exchange_dy_zap.exchange_dy(amm, 1, 0, out_amount, dx - 1)  # ETH --> crvUSD
+        exchange_dy_zap.exchange_dy(amm, 1, 0, out_amount, dx)  # ETH --> crvUSD
     dy_measured = borrowed_token.balanceOf(u) - dy_measured
     dx_measured -= collateral_token.balanceOf(u)
     assert abs(dy_measured - dy) <= borrowed_precision
     assert approx(dx_measured, dx, 5e-5)
-
-
-def test_slippage(amm, exchange_dy_zap, accounts, borrowed_token, collateral_token, admin):
-    collateral_deciamls = collateral_token.decimals()
-    amounts = list(map(lambda x: int(x * 10**collateral_deciamls), [1, 2, 3, 4, 5, 6]))
-    ns = [1, 2, 3, 4, 5, 6]
-    dns = [1, 2, 3, 4, 5, 6]
-
-    with boa.env.prank(admin):
-        for user, amount, n1, dn in zip(accounts[1:6], amounts, ns, dns):
-            n2 = n1 + dn
-            collateral_token._mint_for_testing(user, amount)
-            amm.deposit_range(user, amount, n1, n2, True)
-
-    amount = amount * 10**borrowed_token.decimals()
-    u = accounts[6]
-    dy, dx = exchange_dy_zap.get_dydx(amm, 0, 1, amount)
-
-    borrowed_token._mint_for_testing(u, dx)
-    # with boa.env.prank(u):
-    #     exchange_dy_zap.exchange_dy(amm, 0, 1, amount, 0)  # ETH --> crvUSD
-    with boa.env.prank(u):
-        with boa.reverts("Slippage"):
-            exchange_dy_zap.exchange_dy(amm, 0, 1, dy, 0)  # crvUSD --> ETH
