c156d662a6d230b8b8314dbeb650c8238558377c
Michael Egorov <michwill@gmail.com>
2023-01-06 00:46:58 +0100

    Leverage and deleverage callback works

--- tests/lendborrow/test_leverage.py
+++ tests/lendborrow/test_leverage.py
@@ -21,7 +21,9 @@ def test_leverage(collateral_token, stablecoin, market_controller, market_amm, f
     user = accounts[0]
     amount = 10 * 10**18
     leverage_method = get_method_id("leverage(address,uint256,uint256,uint256[])")  # min_amount for output collateral
-    deleverage_method = get_method_id("leverage(address,uint256,uint256,uint256,uint256[])")  # min_amount for stablecoins
+    deleverage_method = get_method_id("deleverage(address,uint256,uint256,uint256,uint256[])")  # min_amount for stablecoins
+
+    controller_mint = stablecoin.balanceOf(market_controller.address)
 
     with boa.env.prank(user):
         boa.env.set_balance(user, amount)
@@ -35,3 +37,11 @@ def test_leverage(collateral_token, stablecoin, market_controller, market_amm, f
         assert stablecoin.balanceOf(user) == 0
 
         market_controller.repay_extended(fake_leverage.address, deleverage_method, [int(0.8 * amount * 2 * 3000)])
+        assert market_controller.debt(user) == 0
+        assert collateral_token.balanceOf(market_amm.address) == 0
+        assert stablecoin.balanceOf(market_amm.address) == 0
+        assert market_amm.get_sum_xy(user) == (0, 0)
+        assert collateral_token.balanceOf(market_controller.address) == 0
+        assert stablecoin.balanceOf(market_controller.address) == controller_mint
+        assert collateral_token.balanceOf(user) == amount
+        assert stablecoin.balanceOf(user) == 0
