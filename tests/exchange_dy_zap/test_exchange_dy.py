901cc057abb2666c967926f9d49e209016a634ce
Michael Egorov <michwill@gmail.com>
2023-01-15 14:42:27 +0100

    deciamls -> decimals

--- tests/exchange_dy_zap/test_exchange_dy.py
+++ tests/exchange_dy_zap/test_exchange_dy.py
@@ -12,14 +12,14 @@ from datetime import timedelta
 )
 @settings(deadline=timedelta(seconds=1000))
 def test_dydx_limits(amm, amounts, accounts, ns, dns, collateral_token, admin, borrowed_token):
-    collateral_deciamls = collateral_token.decimals()
-    borrowed_deciamls = borrowed_token.decimals()
-    amounts = list(map(lambda x: int(x * 10**collateral_deciamls), amounts))
+    collateral_decimals = collateral_token.decimals()
+    borrowed_decimals = borrowed_token.decimals()
+    amounts = list(map(lambda x: int(x * 10**collateral_decimals), amounts))
 
     # 18 - 10_000, 17 - 1000, 16 - 100, 15 - 10, <= 14 - 1
-    borrowed_precision = 10 ** (max(borrowed_deciamls - 14, 0))
+    borrowed_precision = 10 ** (max(borrowed_decimals - 14, 0))
     # >= 16 - 0; 15, 14 - 10; 13, 12 - 1000 ...
-    collateral_precision = 10 ** (max(15 - (borrowed_deciamls // 2) * 2, 0)) if borrowed_deciamls < 16 else 0
+    collateral_precision = 10 ** (max(15 - (borrowed_decimals // 2) * 2, 0)) if borrowed_decimals < 16 else 0
 
     with boa.env.prank(admin):
         for user, amount, n1, dn in zip(accounts[1:6], amounts, ns, dns):
@@ -34,36 +34,36 @@ def test_dydx_limits(amm, amounts, accounts, ns, dns, collateral_token, admin, b
     assert dx == dy == 0
 
     # Small swap
-    dy1, dx1 = amm.get_dydx(0, 1, 10**(collateral_deciamls - 2))
+    dy1, dx1 = amm.get_dydx(0, 1, 10**(collateral_decimals - 2))
     dx2, dy2 = amm.get_dxdy(0, 1, dx1)
     assert dx1 == dx2
     assert abs(dy1 - dy2) <= collateral_precision
 
-    dx1, dy1 = amm.get_dxdy(0, 1, 10**(borrowed_deciamls - 2))
+    dx1, dy1 = amm.get_dxdy(0, 1, 10**(borrowed_decimals - 2))
     dy2, dx2 = amm.get_dydx(0, 1, dy1)
     assert abs(dx1 - dx2) <= borrowed_precision
     assert dy1 == dy2
 
-    dy, dx = amm.get_dydx(1, 0, 10**(collateral_deciamls - 2))  # No liquidity
+    dy, dx = amm.get_dydx(1, 0, 10**(collateral_decimals - 2))  # No liquidity
     assert dx == 0
     assert dy == 0  # Rounded down
 
     # Huge swap
-    dy1, dx1 = amm.get_dydx(0, 1, 10**12 * 10**collateral_deciamls)
+    dy1, dx1 = amm.get_dydx(0, 1, 10**12 * 10**collateral_decimals)
     dx2, dy2 = amm.get_dxdy(0, 1, dx1)
-    assert dy1 < 10**12 * 10**collateral_deciamls      # Less than all is desired
+    assert dy1 < 10**12 * 10**collateral_decimals      # Less than all is desired
     assert abs(dy1 - sum(amounts)) <= 1000             # but everything is bought
     assert dx1 == dx2
     assert dy1 == dy2
 
-    dx1, dy1 = amm.get_dxdy(0, 1, 10**12 * 10**borrowed_deciamls)
+    dx1, dy1 = amm.get_dxdy(0, 1, 10**12 * 10**borrowed_decimals)
     dy2, dx2 = amm.get_dydx(0, 1, dy1)
-    assert dx1 < 10**12 * 10**borrowed_deciamls        # Less than all is spent
+    assert dx1 < 10**12 * 10**borrowed_decimals        # Less than all is spent
     assert abs(dy1 - sum(amounts)) <= 1000             # but everything is bought
     assert dx1 == dx2
     assert dy1 == dy2
 
-    dy, dx = amm.get_dydx(1, 0, 10**12 * 10**collateral_deciamls)  # No liquidity
+    dy, dx = amm.get_dydx(1, 0, 10**12 * 10**collateral_decimals)  # No liquidity
     assert dx == 0
     assert dy == 0  # Rounded down
 
@@ -76,16 +76,16 @@ def test_dydx_limits(amm, amounts, accounts, ns, dns, collateral_token, admin, b
 )
 @settings(deadline=timedelta(seconds=1000))
 def test_exchange_dy_down_up(amm, amounts, accounts, ns, dns, amount, borrowed_token, collateral_token, admin):
-    collateral_deciamls = collateral_token.decimals()
-    borrowed_deciamls = borrowed_token.decimals()
-    amounts = list(map(lambda x: int(x * 10**collateral_deciamls), amounts))
-    amount = amount * 10**borrowed_deciamls
+    collateral_decimals = collateral_token.decimals()
+    borrowed_decimals = borrowed_token.decimals()
+    amounts = list(map(lambda x: int(x * 10**collateral_decimals), amounts))
+    amount = amount * 10**borrowed_decimals
     u = accounts[6]
 
     # 18 - 10_000, 17 - 1000, 16 - 100, 15 - 10, <= 14 - 1
-    borrowed_precision = 10 ** (max(borrowed_deciamls - 14, 0))
+    borrowed_precision = 10 ** (max(borrowed_decimals - 14, 0))
     # >= 16 - 0; 15, 14 - 10; 13, 12 - 1000 ...
-    collateral_precision = 10 ** (max(15 - (borrowed_deciamls // 2) * 2, 0)) if borrowed_deciamls < 16 else 0
+    collateral_precision = 10 ** (max(15 - (borrowed_decimals // 2) * 2, 0)) if borrowed_decimals < 16 else 0
 
     with boa.env.prank(admin):
         for user, amount, n1, dn in zip(accounts[1:6], amounts, ns, dns):
@@ -112,8 +112,8 @@ def test_exchange_dy_down_up(amm, amounts, accounts, ns, dns, amount, borrowed_t
 
     sum_borrowed = sum(amm.bands_x(i) for i in range(50))
     sum_collateral = sum(amm.bands_y(i) for i in range(50))
-    assert abs(borrowed_token.balanceOf(amm) - sum_borrowed // 10**(18 - borrowed_deciamls)) <= 1
-    assert abs(collateral_token.balanceOf(amm) - sum_collateral // 10**(18 - collateral_deciamls)) <= 1
+    assert abs(borrowed_token.balanceOf(amm) - sum_borrowed // 10**(18 - borrowed_decimals)) <= 1
+    assert abs(collateral_token.balanceOf(amm) - sum_collateral // 10**(18 - collateral_decimals)) <= 1
 
     expected_in_amount = int(dy2 / 0.98)  # two trades charge 1% twice
     out_amount = dx2
