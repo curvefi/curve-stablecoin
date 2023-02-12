a2bde3e4ab44dec0abefbeffde2cf741abc686df
Michael Egorov <michwill@gmail.com>
2023-02-12 22:13:33 +0100

    Property test for leverage and deleverage

--- tests/lendborrow/test_leverage.py
+++ tests/lendborrow/test_leverage.py
@@ -27,7 +27,7 @@ def test_leverage(collateral_token, stablecoin, market_controller, market_amm, f
         assert market_controller.debt(user) == amount * 2 * 3000
         assert stablecoin.balanceOf(user) == 0
 
-        market_controller.repay_extended(fake_leverage.address, deleverage_method, [int(0.8 * amount * 2 * 3000)])
+        market_controller.repay_extended(fake_leverage.address, deleverage_method, [10**18])
         assert market_controller.debt(user) == 0
         assert collateral_token.balanceOf(market_amm.address) == 0
         assert stablecoin.balanceOf(market_amm.address) == 0
@@ -40,10 +40,11 @@ def test_leverage(collateral_token, stablecoin, market_controller, market_amm, f
 
 @given(
     amount=st.integers(min_value=10 * 10**8, max_value=10**18),
-    loan_mul=st.floats(min_value=0, max_value=10.0))
+    loan_mul=st.floats(min_value=0, max_value=10.0),
+    repay_mul=st.floats(min_value=0, max_value=1.0))
 @settings(deadline=timedelta(seconds=1000), max_examples=200)
 def test_leverage_property(collateral_token, stablecoin, market_controller, market_amm, fake_leverage, accounts,
-                           amount, loan_mul):
+                           amount, loan_mul, repay_mul):
     user = accounts[0]
 
     with boa.env.prank(user):
@@ -62,5 +63,30 @@ def test_leverage_property(collateral_token, stablecoin, market_controller, mark
         xy = market_amm.get_sum_xy(user)
         assert xy[0] == 0
         assert approx(xy[1], expected_collateral, 1e-9, 10)
-        assert approx(market_controller.debt(user), int(amount * loan_mul * 3000), 1e-9, 10)
+        assert approx(market_controller.debt(user), debt, 1e-9, 10)
         assert stablecoin.balanceOf(user) == 0
+
+        s0 = stablecoin.balanceOf(market_controller.address)
+
+        if int(debt * repay_mul) > 0:
+            market_controller.repay_extended(fake_leverage.address, deleverage_method, [int(repay_mul * 1e18)])
+        else:
+            with boa.reverts():
+                market_controller.repay_extended(fake_leverage.address, deleverage_method, [int(repay_mul * 1e18)])
+            return
+        assert market_controller.debt(user) == debt - debt * int(repay_mul * 1e18) // 10**18
+        if repay_mul == 1.0:
+            assert collateral_token.balanceOf(market_amm.address) == 0
+            assert stablecoin.balanceOf(market_amm.address) == 0
+            assert market_amm.get_sum_xy(user) == (0, 0)
+        assert collateral_token.balanceOf(market_controller.address) == 0
+        assert stablecoin.balanceOf(market_controller.address) - s0 == debt * int(repay_mul * 1e18) // 10**18
+        if repay_mul == 1.0:
+            assert collateral_token.balanceOf(user) == amount
+        else:
+            assert collateral_token.balanceOf(user) == 0
+
+
+def test_deleverage_error(collateral_token, stablecoin, market_controller, market_amm, fake_leverage, accounts):
+    test_leverage_property.hypothesis.inner_test(
+            collateral_token, stablecoin, market_controller, market_amm, fake_leverage, accounts, 1000000000, 1.0, 0.5)
