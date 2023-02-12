6c4fb49424e23f5bccecdc2d1b009c9402f267e1
Michael Egorov <michwill@gmail.com>
2023-02-12 20:39:32 +0100

    Property test for leverage

--- tests/lendborrow/conftest.py
+++ tests/lendborrow/conftest.py
@@ -101,6 +101,5 @@ def fake_leverage(stablecoin, collateral_token, market_controller, admin):
     with boa.env.prank(admin):
         leverage = boa.load('contracts/testing/FakeLeverage.vy', stablecoin.address, collateral_token.address,
                             market_controller.address, 3000 * 10**18)
-        boa.env.set_balance(admin, 1000 * 10**18)
         collateral_token._mint_for_testing(leverage.address, 1000 * 10**18)
         return leverage
--- tests/lendborrow/test_leverage.py
+++ tests/lendborrow/test_leverage.py
@@ -1,17 +1,23 @@
 import boa
+from datetime import timedelta
 from .conftest import get_method_id
+from ..conftest import approx
+
+from hypothesis import given, settings
+from hypothesis import strategies as st
+
+
+leverage_method = get_method_id("leverage(address,uint256,uint256,uint256,uint256[])")  # min_amount for output collateral
+deleverage_method = get_method_id("deleverage(address,uint256,uint256,uint256,uint256[])")  # min_amount for stablecoins
 
 
 def test_leverage(collateral_token, stablecoin, market_controller, market_amm, fake_leverage, accounts):
     user = accounts[0]
     amount = 10 * 10**18
-    leverage_method = get_method_id("leverage(address,uint256,uint256,uint256,uint256[])")  # min_amount for output collateral
-    deleverage_method = get_method_id("deleverage(address,uint256,uint256,uint256,uint256[])")  # min_amount for stablecoins
 
     controller_mint = stablecoin.balanceOf(market_controller.address)
 
     with boa.env.prank(user):
-        boa.env.set_balance(user, amount)
         collateral_token._mint_for_testing(user, amount)
 
         market_controller.create_loan_extended(amount, amount * 2 * 3000, 5, fake_leverage.address, leverage_method, [int(amount * 1.5)])
@@ -29,4 +35,32 @@ def test_leverage(collateral_token, stablecoin, market_controller, market_amm, f
         assert collateral_token.balanceOf(market_controller.address) == 0
         assert stablecoin.balanceOf(market_controller.address) == controller_mint
         assert collateral_token.balanceOf(user) == amount
+        assert stablecoin.balanceOf(user) == 0
+
+
+@given(
+    amount=st.integers(min_value=10 * 10**8, max_value=10**18),
+    loan_mul=st.floats(min_value=0, max_value=10.0))
+@settings(deadline=timedelta(seconds=1000), max_examples=200)
+def test_leverage_property(collateral_token, stablecoin, market_controller, market_amm, fake_leverage, accounts,
+                           amount, loan_mul):
+    user = accounts[0]
+
+    with boa.env.prank(user):
+        collateral_token._mint_for_testing(user, amount)
+
+        debt = int(loan_mul * amount * 3000)
+        if (debt // 3000) <= collateral_token.balanceOf(fake_leverage.address) and debt > 0:
+            market_controller.create_loan_extended(amount, debt, 5, fake_leverage.address, leverage_method, [0])
+        else:
+            with boa.reverts():
+                market_controller.create_loan_extended(amount, debt, 5, fake_leverage.address, leverage_method, [0])
+            return
+        assert collateral_token.balanceOf(user) == 0
+        expected_collateral = int((1 + loan_mul) * amount)
+        assert approx(collateral_token.balanceOf(market_amm.address), expected_collateral, 1e-9, 10)
+        xy = market_amm.get_sum_xy(user)
+        assert xy[0] == 0
+        assert approx(xy[1], expected_collateral, 1e-9, 10)
+        assert approx(market_controller.debt(user), int(amount * loan_mul * 3000), 1e-9, 10)
         assert stablecoin.balanceOf(user) == 0
