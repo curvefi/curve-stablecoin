3cd358d4d64560aea0e4bc03a729108a8c2ab72c
Michael Egorov <michwill@gmail.com>
2023-04-06 02:30:50 +0200

    Script to use real DAO addresses and WETH when it's mainnet[-fork]

--- scripts/ape-deploy.py
+++ scripts/ape-deploy.py
@@ -1,4 +1,4 @@
-from ape import project, accounts
+from ape import project, accounts, Contract
 from ape.cli import NetworkBoundCommand, network_option
 # account_option could be used when in prod?
 import click
@@ -40,36 +40,51 @@ def cli():
 )
 @network_option()
 def deploy(network):
-    if 'mainnet' in network:
-        # admin = fee_receiver = accounts.load('babe')
-        raise NotImplementedError("Mainnet not implemented yet")
-    else:
+    # Select appropriate account
+    if ':local:' in network or ':mainnet-fork:' in network:
         account = accounts.test_accounts[0]
+    elif ':mainnet:' in network:
+        account = accounts.load('babe')
+
+    temporary_admin = account
+
+    if ':local:' in network:
         admin = account
         fee_receiver = account
+    elif 'mainnet' in network:
+        admin = '0x40907540d8a6C65c637785e8f8B742ae6b0b9968'  # Ownership admin
+        fee_receiver = '0xeCb456EA5365865EbAb8a2661B0c503410e9B347'  # 0xECB for fee collection
+
+    if ':local:' in network:
         weth = account.deploy(project.WETH)
+    elif 'mainnet' in network:
+        weth = Contract("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")
 
-    stablecoin = account.deploy(project.Stablecoin, FULL_NAME, SHORT_NAME)
-    factory = account.deploy(project.ControllerFactory, stablecoin, admin, weth, fee_receiver)
+    with accounts.use_sender(account):
+        stablecoin = account.deploy(project.Stablecoin, FULL_NAME, SHORT_NAME)
+        factory = account.deploy(project.ControllerFactory, stablecoin, temporary_admin, weth, fee_receiver)
 
-    controller_impl = deploy_blueprint(project.Controller, account)
-    amm_impl = deploy_blueprint(project.AMM, account)
+        controller_impl = deploy_blueprint(project.Controller, account)
+        amm_impl = deploy_blueprint(project.AMM, account)
 
-    factory.set_implementations(controller_impl, amm_impl, sender=account)
-    stablecoin.set_minter(factory.address, sender=account)
+        factory.set_implementations(controller_impl, amm_impl)
+        stablecoin.set_minter(factory)
 
-    if 'mainnet' not in network:
-        policy = account.deploy(project.ConstantMonetaryPolicy, admin)
-        policy.set_rate(0, sender=account)  # 0%
-        price_oracle = account.deploy(project.DummyPriceOracle, admin, 3000 * 10**18)
+        if 'mainnet-fork' in network or 'local' in network:
+            policy = account.deploy(project.ConstantMonetaryPolicy, temporary_admin)
+            policy.set_rate(0)  # 0%
+            price_oracle = account.deploy(project.DummyPriceOracle, admin, 3000 * 10**18)
 
-    factory.add_market(
-        weth, 100, 10**16, 0,
-        price_oracle,
-        policy, 5 * 10**16, 2 * 10**16,
-        10**6 * 10**18,
-        sender=account
-    )
+        factory.add_market(
+            weth, 100, 10**16, 0,
+            price_oracle,
+            policy, 5 * 10**16, 2 * 10**16,
+            10**6 * 10**18
+        )
+
+        if admin != temporary_admin:
+            policy.set_admin(admin)
+            factory.set_admin(admin)
 
     amm = project.AMM.at(factory.get_amm(weth))
     controller = project.Controller.at(factory.get_controller(weth))
