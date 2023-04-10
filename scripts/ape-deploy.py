e3f8f4a20283a5d6ca3f22b73c95f45d418ea5b8
Michael Egorov <michwill@gmail.com>
2023-04-11 00:26:42 +0200

    Use .env config with ethereum rpc keys etc

--- scripts/ape-deploy.py
+++ scripts/ape-deploy.py
@@ -3,6 +3,11 @@ from ape.cli import NetworkBoundCommand, network_option
 # account_option could be used when in prod?
 import click
 
+from dotenv import load_dotenv
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(Path(BASE_DIR, ".env"))
+
 SHORT_NAME = "crvUSD"
 FULL_NAME = "Curve.Fi USD Stablecoin"
 
