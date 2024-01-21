3f4d1d78901876a9a6a9a4c91580a5ab839257bf
Michael Egorov <michwill@gmail.com>
2024-01-21 23:57:19 +0100

    Fixtures for testing 2-way vaults

--- tests/lending/conftest.py
+++ tests/lending/conftest.py
@@ -70,7 +70,7 @@ def factory(stablecoin, amm_impl, controller_impl, vault_impl, price_oracle_impl
 
 
 @pytest.fixture(scope="module", params=product([2, 6, 8, 18], [True, False]))
-def vault(factory, vault_impl, get_collateral_token, stablecoin, price_oracle, admin, request):
+def tokens_for_vault(get_collateral_token, stablecoin, request):
     decimals, stablecoin_is_borrowed = request.param
     token = get_collateral_token(decimals)
     if stablecoin_is_borrowed:
@@ -79,6 +79,21 @@ def vault(factory, vault_impl, get_collateral_token, stablecoin, price_oracle, a
     else:
         borrowed_token = token
         collateral_token = stablecoin
+    return borrowed_token, collateral_token
+
+
+@pytest.fixture(scope="module")
+def collateral_token(tokens_for_vault):
+    return tokens_for_vault[1]
+
+
+@pytest.fixture(scope="module")
+def borrowed_token(tokens_for_vault):
+    return tokens_for_vault[0]
+
+
+@pytest.fixture(scope="module")
+def vault(factory, vault_impl, collateral_token, borrowed_token, price_oracle, admin):
     with boa.env.prank(admin):
         return vault_impl.at(
             factory.create(
@@ -110,16 +125,6 @@ def mock_token_interface():
 
 
 @pytest.fixture(scope="module")
-def borrowed_token(vault, mock_token_interface):
-    return mock_token_interface.at(vault.borrowed_token())
-
-
-@pytest.fixture(scope="module")
-def collateral_token(vault, mock_token_interface):
-    return mock_token_interface.at(vault.collateral_token())
-
-
-@pytest.fixture(scope="module")
 def filled_controller(vault, borrowed_token, market_controller, admin):
     with boa.env.prank(admin):
         amount = 100 * 10**6 * 10**(borrowed_token.decimals())
@@ -136,3 +141,65 @@ def fake_leverage(collateral_token, borrowed_token, market_controller, admin):
                             market_controller.address, 3000 * 10**18)
         collateral_token._mint_for_testing(leverage.address, 1000 * 10**collateral_token.decimals())
         return leverage
+
+
+@pytest.fixture(scope="module")
+def vault_price_oracle_impl(admin):
+    with boa.env.prank(admin):
+        return boa.load_partial('contracts/price_oracles/CryptoFromPoolVault.vy').deploy_as_blueprint()
+
+
+@pytest.fixture(scope="module")
+def wrapper_oracle_impl(admin):
+    with boa.env.prank(admin):
+        return boa.load_partial('contracts/price_oracles/OracleVaultWrapper.vy').deploy_as_blueprint()
+
+
+@pytest.fixture(scope="module")
+def factory_2way(stablecoin, amm_impl, controller_impl, vault_impl, vault_price_oracle_impl, wrapper_oracle_impl,
+                 mpolicy_impl, admin):
+    with boa.env.prank(admin):
+        return boa.load(
+            'contracts/lending/TwoWayLendingFactory.vy',
+            stablecoin, amm_impl, controller_impl, vault_impl,
+            vault_price_oracle_impl, wrapper_oracle_impl, mpolicy_impl, admin)
+
+
+@pytest.fixture(scope="module")
+def vaults_2way(factory_2way, vault_impl, collateral_token, borrowed_token, price_oracle, admin):
+    with boa.env.prank(admin):
+        vault_long, vault_short = factory_2way.create(
+            borrowed_token.address, collateral_token.address,
+            100, int(0.006 * 1e18), int(0.09 * 1e18), int(0.06 * 1e18),
+            price_oracle.address)
+        return vault_impl.at(vault_long), vault_impl.at(vault_short)
+
+
+@pytest.fixture(scope="module")
+def vault_long(vaults_2way):
+    return vaults_2way[0]
+
+
+@pytest.fixture(scope="module")
+def vault_short(vaults_2way):
+    return vaults_2way[1]
+
+
+@pytest.fixture(scope="module")
+def controller_long(vault_long, controller_interface):
+    return controller_interface.at(vault_long.controller())
+
+
+@pytest.fixture(scope="module")
+def amm_long(vault_long, amm_interface):
+    return amm_interface.at(vault_long.amm())
+
+
+@pytest.fixture(scope="module")
+def controller_short(vault_short, controller_interface):
+    return controller_interface.at(vault_short.controller())
+
+
+@pytest.fixture(scope="module")
+def amm_short(vault_short, amm_interface):
+    return amm_interface.at(vault_short.amm())
---
+++ tests/lending/test_twoway_vault.py
@@ -0,0 +1,4 @@
+def test_vault_creation(vault_long, vault_short,
+                        controller_long, controller_short, amm_long, amm_short,
+                        collateral_token, borrowed_token, price_oracle):
+    pass
