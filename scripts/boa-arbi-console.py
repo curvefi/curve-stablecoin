4c38a6ad7525a9f0dbda454d483505db57555bbe
Michael Egorov <michwill@gmail.com>
2024-03-22 22:23:52 +1100

    Console for deploying things (for debug) on arbi

---
+++ scripts/boa-arbi-console.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import boa
+import json
+import os
+import sys
+from getpass import getpass
+from eth_account import account
+from networks import ARBITRUM
+
+
+def account_load(fname):
+    path = os.path.expanduser(os.path.join('~', '.brownie', 'accounts', fname + '.json'))
+    with open(path, 'r') as f:
+        pkey = account.decode_keyfile_json(json.load(f), getpass())
+        return account.Account.from_key(pkey)
+
+
+if __name__ == '__main__':
+    if '--fork' in sys.argv[1:]:
+        boa.env.fork(ARBITRUM)
+        boa.env.eoa = '0xbabe61887f1de2713c6f97e567623453d3C79f67'
+    else:
+        boa.set_network_env(ARBITRUM)
+        boa.env.add_account(account_load('babe'))
+        boa.env._fork_try_prefetch_state = False
+
+    oracle_code = boa.load_partial('contracts/price_oracles/L2/CryptoFromPoolArbitrum.vy')
+
+    import IPython
+    IPython.embed()
