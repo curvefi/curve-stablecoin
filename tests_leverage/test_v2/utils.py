ab65fbb86960d4e3f59e3fb85ef65f23c5dc078d
Oleg <40476427+amfet42@users.noreply.github.com>
2024-06-03 20:07:58 +0300

    add tests for leverage

--- tests_leverage/test_v2/utils.py
+++ tests_leverage/test_v2/utils.py
@@ -1,3 +1,5 @@
+import time
+
 import boa
 import requests
 
@@ -6,17 +8,41 @@ from .settings import EXPLORER_TOKEN, EXPLORER_URL, ROUTER_1INCH_TOKEN
 
 
 def get_contract_from_explorer(address: str):
-    return boa.from_etherscan(address=address, name=address, uri=EXPLORER_URL, api_key=EXPLORER_TOKEN)
+    return boa.from_etherscan(
+        address=address, name=address, uri=EXPLORER_URL, api_key=EXPLORER_TOKEN
+    )
 
 
 class Router1inch:
     def __init__(self, chain_id: int):
         self._chain_id = chain_id
         self._headers = {"Authorization": f"Bearer {ROUTER_1INCH_TOKEN}"}
-        self._from = CRVUSD
+        self._crvusd = CRVUSD
 
-    def get_rate(self, _to: str, amount: int):
+    def get_rate(self, _from: str, _to: str, amount: int):
         url = "https://api.1inch.dev/swap/v6.0/1/quote"
-        params = {"src": self._from, "dst": _to, "amount": amount}
+        params = {"src": _from, "dst": _to, "amount": amount}
         resp = requests.get(url, headers=self._headers, params=params)
+        time.sleep(1)
         return int(resp.json()["dstAmount"])
+
+    def get_rate_to_crvusd(self, _from: str, amount: int):
+        return self.get_rate(_from, CRVUSD, amount)
+
+    def get_rate_from_crvusd(self, _to: str, amount: int):
+        return self.get_rate(CRVUSD, _to, amount)
+
+    def get_calldata(self, _from: str, _to: str, amount: int, user: str) -> str:
+        url = "https://api.1inch.dev/swap/v6.0/1/swap"
+        params = {
+            "src": _from,
+            "dst": _to,
+            "amount": amount,
+            "from": user,
+            "slippage": 0.1,
+            "disableEstimate": True,
+        }
+        resp = requests.get(url, headers=self._headers, params=params)
+
+        time.sleep(1)
+        return resp.json()["tx"]["data"]
