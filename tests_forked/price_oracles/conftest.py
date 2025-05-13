9e819bd631e40d1eaed02214f635d64b71bc8ea0
macket <macket24@proton.me>
2025-05-13 12:21:58 +0400

    fix: test fork

--- tests_forked/price_oracles/conftest.py
+++ tests_forked/price_oracles/conftest.py
@@ -3,12 +3,12 @@ import pytest
 from .settings import WEB3_PROVIDER_URL, EXPLORER_URL, EXPLORER_TOKEN
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(scope="module", autouse=True)
 def boa_fork():
     assert WEB3_PROVIDER_URL is not None, "Provider url is not set, add WEB3_PROVIDER_URL param to env"
-    boa.env.fork(url=WEB3_PROVIDER_URL)
+    boa.fork(WEB3_PROVIDER_URL)
 
 
-@pytest.fixture(scope="module", autouse=True)
+@pytest.fixture(scope="module")
 def stablecoin_aggregator():
     return boa.from_etherscan("0x18672b1b0c623a30089A280Ed9256379fb0E4E62", "AggregatorStablePrice", uri=EXPLORER_URL, api_key=EXPLORER_TOKEN)  # USD/crvUSD
