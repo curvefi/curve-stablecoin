d28954b608c60202c36bcf6a92c75448d8276eb7
Alberto <albicento.ac@gmail.com>
2025-09-01 12:35:07 +0200

    test: simplify flashloan tests

--- tests/flashloan/conftest.py
+++ tests/flashloan/conftest.py
@@ -1,68 +1,15 @@
 import boa
 import pytest
 from tests.utils.deployers import (
-    STABLECOIN_DEPLOYER,
-    WETH_DEPLOYER,
-    CONTROLLER_FACTORY_DEPLOYER,
     FLASH_LENDER_DEPLOYER,
-    AMM_DEPLOYER,
     DUMMY_FLASH_BORROWER_DEPLOYER
 )
 
 
-@pytest.fixture(scope="session")
-def stablecoin_pre():
-    return STABLECOIN_DEPLOYER
-
-
 @pytest.fixture(scope="module")
-def stablecoin(stablecoin_pre, admin):
-    with boa.env.prank(admin):
-        _stablecoin = stablecoin_pre.deploy('Curve USD', 'crvUSD')
-        _stablecoin.mint(admin, 10**21)
-        return _stablecoin
-
-
-@pytest.fixture(scope="session")
-def weth(admin):
-    with boa.env.prank(admin):
-        return WETH_DEPLOYER.deploy()
-
-
-@pytest.fixture(scope="session")
-def controller_factory_impl():
-    return CONTROLLER_FACTORY_DEPLOYER
-
-
-@pytest.fixture(scope="module")
-def controller_prefactory(controller_factory_impl, stablecoin, weth, admin, accounts):
-    with boa.env.prank(admin):
-        return controller_factory_impl.deploy(stablecoin.address, admin, accounts[0], weth.address)
-
-
-@pytest.fixture(scope="session")
-def controller_impl(admin):
-    with boa.env.prank(admin):
-        return FLASH_LENDER_DEPLOYER.deploy_as_blueprint()
-
-
-@pytest.fixture(scope="session")
-def amm_impl(admin):
-    with boa.env.prank(admin):
-        return AMM_DEPLOYER.deploy_as_blueprint()
-
-
-@pytest.fixture(scope="session")
-def user():
-    return boa.env.generate_address()
-
-
-@pytest.fixture(scope="module")
-def controller_factory(controller_prefactory, amm_impl, controller_impl, stablecoin, admin):
-    with boa.env.prank(admin):
-        controller_prefactory.set_implementations(controller_impl.address, amm_impl.address)
-        stablecoin.set_minter(controller_prefactory.address)
-    return controller_prefactory
+def controller_factory(proto):
+    """Return the preconfigured mint factory from Protocol."""
+    return proto.mint_factory
 
 
 @pytest.fixture(scope="module")
@@ -75,7 +22,6 @@ def flash_lender(controller_factory, admin, max_flash_loan):
     with boa.env.prank(admin):
         fl = FLASH_LENDER_DEPLOYER.deploy(controller_factory.address)
         controller_factory.set_debt_ceiling(fl.address, max_flash_loan)
-
         return fl
 
 
--- tests/flashloan/test_debt_ceiling.py
+++ tests/flashloan/test_debt_ceiling.py
@@ -32,39 +32,36 @@ def test_empty_flash_lender(controller_factory, flash_lender, stablecoin, max_fl
 
 
 def test_increase_debt_ceiling_with_excess(controller_factory, flash_lender, stablecoin, max_flash_loan, admin):
-    with boa.env.prank(admin):
-        stablecoin.transfer(flash_lender, 10**21)
+    boa.deal(stablecoin, flash_lender, stablecoin.balanceOf(flash_lender) + 10**21)
 
-        assert controller_factory.debt_ceiling_residual(flash_lender) == max_flash_loan
-        assert stablecoin.balanceOf(flash_lender) == max_flash_loan + 10**21
+    assert controller_factory.debt_ceiling_residual(flash_lender) == max_flash_loan
+    assert stablecoin.balanceOf(flash_lender) == max_flash_loan + 10**21
 
-        controller_factory.set_debt_ceiling(flash_lender, max_flash_loan * 2)
+    controller_factory.set_debt_ceiling(flash_lender, max_flash_loan * 2, sender=admin)
 
-        assert controller_factory.debt_ceiling_residual(flash_lender) == max_flash_loan * 2
-        assert stablecoin.balanceOf(flash_lender) == max_flash_loan * 2 + 10**21
+    assert controller_factory.debt_ceiling_residual(flash_lender) == max_flash_loan * 2
+    assert stablecoin.balanceOf(flash_lender) == max_flash_loan * 2 + 10**21
 
 
 def test_decrease_debt_ceiling_with_excess(controller_factory, flash_lender, stablecoin, max_flash_loan, admin):
-    with boa.env.prank(admin):
-        stablecoin.transfer(flash_lender, 10**21)
+    boa.deal(stablecoin, flash_lender, stablecoin.balanceOf(flash_lender) + 10**21)
 
-        assert controller_factory.debt_ceiling_residual(flash_lender) == max_flash_loan
-        assert stablecoin.balanceOf(flash_lender) == max_flash_loan + 10**21
+    assert controller_factory.debt_ceiling_residual(flash_lender) == max_flash_loan
+    assert stablecoin.balanceOf(flash_lender) == max_flash_loan + 10**21
 
-        controller_factory.set_debt_ceiling(flash_lender, max_flash_loan // 2, sender=admin)
+    controller_factory.set_debt_ceiling(flash_lender, max_flash_loan // 2, sender=admin)
 
-        assert controller_factory.debt_ceiling_residual(flash_lender) == max_flash_loan // 2
-        assert stablecoin.balanceOf(flash_lender) == max_flash_loan // 2 + 10**21
+    assert controller_factory.debt_ceiling_residual(flash_lender) == max_flash_loan // 2
+    assert stablecoin.balanceOf(flash_lender) == max_flash_loan // 2 + 10**21
 
 
 def test_empty_flash_lender_with_excess(controller_factory, flash_lender, stablecoin, max_flash_loan, admin):
-    with boa.env.prank(admin):
-        stablecoin.transfer(flash_lender, 10**21)
+    boa.deal(stablecoin, flash_lender, stablecoin.balanceOf(flash_lender) + 10**21)
 
-        assert controller_factory.debt_ceiling_residual(flash_lender) == max_flash_loan
-        assert stablecoin.balanceOf(flash_lender) == max_flash_loan + 10**21
+    assert controller_factory.debt_ceiling_residual(flash_lender) == max_flash_loan
+    assert stablecoin.balanceOf(flash_lender) == max_flash_loan + 10**21
 
-        controller_factory.set_debt_ceiling(flash_lender, 0, sender=admin)
+    controller_factory.set_debt_ceiling(flash_lender, 0, sender=admin)
 
-        assert controller_factory.debt_ceiling_residual(flash_lender) == 0
-        assert stablecoin.balanceOf(flash_lender) == 10**21
+    assert controller_factory.debt_ceiling_residual(flash_lender) == 0
+    assert stablecoin.balanceOf(flash_lender) == 10**21
