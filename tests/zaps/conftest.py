d3228f8625db225837f30f771a1115dedeb89e38
Alberto <albicento.ac@gmail.com>
2025-09-01 14:52:05 +0200

    test: simplify zap fixture

--- tests/zaps/conftest.py
+++ tests/zaps/conftest.py
@@ -1,7 +1,7 @@
 import boa
 import pytest
 
-from tests.utils.deploy import Blueprints, Protocol
+from tests.utils.deploy import Protocol
 
 from tests.utils.deployers import (
     # Core contracts
@@ -44,71 +44,10 @@ def borrowed_token(tokens_for_vault):
 
 
 @pytest.fixture(scope="module")
-def lending_market(borrowed_token, collateral_token, price_oracle, admin):
-
-    class ProtocolWithConstantMP(Protocol):
-        def __init__(self, initial_price: int = 3000 * 10**18):
-            self.admin = admin
-            self.fee_receiver = admin
-
-            # Deploy all blueprints
-            self.blueprints = Blueprints(
-                amm=AMM_DEPLOYER,
-                mint_controller=MINT_CONTROLLER_DEPLOYER,
-                ll_controller=LL_CONTROLLER_DEPLOYER,
-                ll_controller_view=LL_CONTROLLER_VIEW_DEPLOYER,
-                price_oracle=CRYPTO_FROM_POOL_DEPLOYER,
-                mpolicy=CONSTANT_MONETARY_POLICY_LENDING_DEPLOYER,
-            )
-
-            # Deploy core infrastructure
-            with boa.env.prank(self.admin):
-                # Deploy stablecoin
-                self.crvUSD = STABLECOIN_DEPLOYER.deploy("Curve USD", "crvUSD")
-                self.__init_mint_markets(initial_price)
-                self.__init_lend_markets()
-
-        def __init_mint_markets(self, initial_price):
-            # Deploy WETH
-            self.weth = WETH_DEPLOYER.deploy()
-
-            # Deploy a dummy price oracle for testing
-            self.price_oracle = DUMMY_PRICE_ORACLE_DEPLOYER.deploy(self.admin, initial_price)
-
-            # Deploy Mint Protocol
-            # Deploy controller factory
-            self.mint_factory = CONTROLLER_FACTORY_DEPLOYER.deploy(
-                self.crvUSD.address, self.admin, self.fee_receiver, self.weth.address
-            )
-
-            # Set implementations on factory using blueprints
-            self.mint_factory.set_implementations(self.blueprints.mint_controller.address, self.blueprints.amm.address)
-
-            # Set stablecoin minter to factory
-            self.crvUSD.set_minter(self.mint_factory.address)
-
-            # Deploy monetary policy for mint markets
-            self.mint_monetary_policy = CONSTANT_MONETARY_POLICY_DEPLOYER.deploy(self.admin)
-
-        def __init_lend_markets(self):
-            # Deploy Lending Protocol
-            # Deploy vault implementation
-            self.vault_impl = VAULT_DEPLOYER.deploy()
-
-            # Deploy lending factory
-            self.lending_factory = LENDING_FACTORY_DEPLOYER.deploy(
-                self.blueprints.amm.address,
-                self.blueprints.ll_controller.address,
-                self.vault_impl.address,
-                self.blueprints.price_oracle.address,
-                self.blueprints.ll_controller_view.address,
-                self.blueprints.mpolicy.address,
-                self.admin,
-                self.fee_receiver,
-            )
-
+def lending_market(proto, borrowed_token, collateral_token, price_oracle, admin):
+    """Create a lending market using a constant-rate lending policy via Protocol hook."""
     with boa.env.prank(admin):
-        result = ProtocolWithConstantMP().create_lending_market(
+        return proto.create_lending_market(
             borrowed_token=borrowed_token,
             collateral_token=collateral_token,
             A=100,
@@ -119,8 +58,8 @@ def lending_market(borrowed_token, collateral_token, price_oracle, admin):
             name="Test vault",
             min_borrow_rate=int(0.005 * 1e18) // (365 * 86400),  # 0.5% APR
             max_borrow_rate=int(0.5 * 1e18) // (365 * 86400),  # 50% APR
+            mpolicy_deployer=CONSTANT_MONETARY_POLICY_LENDING_DEPLOYER,
         )
-        return result
 
 
 @pytest.fixture(scope="module")
