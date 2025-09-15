1245f9c02a6bd03e41a9e1d4dc4ded881798b19a
macket <macket24@proton.me>
2025-09-15 15:24:53 +0400

    test: refactor test_amount_for_price

--- tests/amm/test_amount_for_price.py
+++ tests/amm/test_amount_for_price.py
@@ -28,15 +28,13 @@ def test_amount_for_price(price_oracle, amm, accounts, collateral_token, borrowe
         amm.deposit_range(user, deposit_amount, n1, n2)
         mint_for_testing(collateral_token, amm.address, deposit_amount)
 
-    prices = [oracle_price]
-    prices.append(amm.get_p())
-
     with boa.env.prank(user):
         # Dump some to be somewhere inside the bands
         eamount = int(deposit_amount * amm.get_p() // 10**18 * init_trade_frac)
         if eamount > 0:
             mint_for_testing(borrowed_token, user, eamount)
         boa.env.time_travel(600)  # To reset the prev p_o counter
+
         amm.exchange(0, 1, eamount, 0)
         n0 = amm.active_band()
 
@@ -58,44 +56,39 @@ def test_amount_for_price(price_oracle, amm, accounts, collateral_token, borrowe
             amm.exchange(1, 0, amount, 0)
 
     p = amm.get_p()
-    prices.append(p)
-
-    prec = 1e-6
-    if amount > 0:
-        if is_pump:
-            prec += 2 / amount + 2 / (1e12 * amount * 1e18 / p_max)
-        else:
-            prec += 2 / amount + 2 / (amount * p_max / 1e18 / 1e12)
-    else:
-        return
-    prec = max(prec, 1e-3)
-
     n_final = amm.active_band()
 
-    assert p_max == pytest.approx(amm.p_current_up(n2), rel=1e-8)
-    assert p_min == pytest.approx(amm.p_current_down(n1), rel=1e-8)
+    if eamount > 0:
+        assert abs(n_final - n0) < 50
 
-    if abs(n_final - n0) < 50 - 1:
+    if p_final > p_max:
+        p_final = p_max
+    if p_final < p_min:
+        p_final = p_min
+
+    if p == pytest.approx(p_final, rel=1e-3):
+        assert n1 <= n_final <= n2
+    else:
         A = amm.A()
         a_ratio = A / (A - 1)
-        p_o_ratio = amm.p_oracle_up(n_final) / amm.price_oracle()
         if is_pump:
-            if p_o_ratio < a_ratio**-50 * (1 + 1e-8):
-                return
+            if p_min == p_final:
+                # The AMM gives the wrong price based on n0 band. It should be DUMP, not PUMP
+                assert n_final == n0 and amount == 0
+            else:
+                # Nothing to pump for OR too far to pump
+                _n_final = max(n_final, n1)
+                p_o_ratio = amm.p_oracle_up(_n_final) / oracle_price
+                assert collateral_token.balanceOf(amm) == 0 or p_o_ratio < a_ratio ** -50 * (1 + 1e-8)
         else:
-            if p_o_ratio > a_ratio**50 * (1 - 1e-8):
-                return
-
-        if p_final > p_min * (1 + prec) and p_final < p_max * (1 - prec):
-            assert p == pytest.approx(p_final, rel=prec)
-
-        elif p_final >= p_max * (1 - prec):
-            if not p == pytest.approx(p_max, rel=prec):
-                assert n_final > n2
-
-        elif p_final <= p_min * (1 + prec):
-            if not p == pytest.approx(p_min, rel=prec):
-                assert n_final < n1
+            if p_max == p_final:
+                # The AMM gives the wrong price based on n0 band. It should be PUMP, not DUMP
+                assert n_final == n0 and amount == 0
+            else:
+                # Nothing to dump for OR too far to dump
+                _n_final = min(n_final, n2)
+                p_o_ratio = amm.p_oracle_up(_n_final) / oracle_price
+                assert borrowed_token.balanceOf(amm) == 0 or p_o_ratio > a_ratio**50 * (1 - 1e-8)
 
 
 def test_amount_for_price_ticks_too_far(price_oracle, amm, accounts, collateral_token, borrowed_token, admin):
