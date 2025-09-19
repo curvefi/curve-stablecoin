e1e480d7a171f76c2062b07b045c211bc5edcab5
Alberto <albicento.ac@gmail.com>
2025-09-19 15:50:31 +0200

    test: strict is implicit

--- tests/unitary/lending/ll_controller/test_set_borrow_cap.py
+++ tests/unitary/lending/ll_controller/test_set_borrow_cap.py
@@ -6,7 +6,7 @@ from tests.utils import filter_logs
 @pytest.mark.parametrize("new_cap", [0, 12345])
 def test_default_behavior(controller, admin, new_cap):
     controller.set_borrow_cap(new_cap, sender=admin)
-    logs = filter_logs(controller, "SetBorrowCap", _strict=True)
+    logs = filter_logs(controller, "SetBorrowCap")
     assert len(logs) == 1 and logs[-1].borrow_cap == new_cap
     assert controller.borrow_cap() == new_cap
 
