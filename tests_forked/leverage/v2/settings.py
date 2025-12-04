0c1b7b3ac99284dacf69356aa0797c3a9a896278
Alberto <albicento.ac@gmail.com>
2025-12-04 15:40:08 +0100

    chore: move files in correct folders

--- tests_forked/leverage/v2/settings.py
+++
@@ -1,6 +0,0 @@
-import os
-
-ROUTER_1INCH_TOKEN = os.getenv("ROUTER_TOKEN")
-WEB3_PROVIDER_URL = os.getenv("WEB3_PROVIDER_URL", "http://127.0.0.1:8545")
-EXPLORER_URL = os.getenv("EXPLORER_URL", "https://api.etherscan.io/api")
-EXPLORER_TOKEN = os.getenv("EXPLORER_TOKEN")
