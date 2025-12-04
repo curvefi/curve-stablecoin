467a16bd1048b4c8afc91b33276ce9d9f6688812
Alberto <albicento.ac@gmail.com>
2025-12-04 15:09:48 +0100

    chore: lint

--- tests_forked/leverage/v1/test_deleverage.py
+++ tests_forked/leverage/v1/test_deleverage.py
@@ -7,30 +7,44 @@ from hypothesis import strategies as st
     collateral_amt=st.floats(min_value=0.1, max_value=1000),
     repay_frac=st.floats(min_value=0.1, max_value=1),
 )
-@pytest.mark.parametrize("collateral_token", ["sfrxETH", "wstETH", "WBTC", "WETH", "sfrxETH2", "tBTC"])
+@pytest.mark.parametrize(
+    "collateral_token", ["sfrxETH", "wstETH", "WBTC", "WETH", "sfrxETH2", "tBTC"]
+)
 @pytest.mark.parametrize("route_idx", [0, 1, 2, 3, 4])
 def test_deleverage(
-        collaterals,
-        controllers,
-        llammas,
-        leverage_zaps,
-        deleverage_zaps,
-        user,
-        stablecoin_token,
-        collateral_token,
-        collateral_amt,
-        repay_frac,
-        route_idx, ):
+    collaterals,
+    controllers,
+    llammas,
+    leverage_zaps,
+    deleverage_zaps,
+    user,
+    stablecoin_token,
+    collateral_token,
+    collateral_amt,
+    repay_frac,
+    route_idx,
+):
     # 1. Create leveraged position
 
     N = 10
     decimals = collaterals[collateral_token].decimals()
-    balance = user.balance if collateral_token == "WETH" else collaterals[collateral_token].balanceOf(user)
+    balance = (
+        user.balance
+        if collateral_token == "WETH"
+        else collaterals[collateral_token].balanceOf(user)
+    )
     _collateral_amt = min(int(collateral_amt * 10**decimals), balance)
-    max_borrowable, _ = leverage_zaps[collateral_token].max_borrowable_and_collateral(_collateral_amt, N, route_idx)
+    max_borrowable, _ = leverage_zaps[collateral_token].max_borrowable_and_collateral(
+        _collateral_amt, N, route_idx
+    )
     borrow_amt = int(max_borrowable * 0.3)
-    _n1 = leverage_zaps[collateral_token].calculate_debt_n1(_collateral_amt, borrow_amt, N, route_idx)
-    min_recv = int(leverage_zaps[collateral_token].get_collateral_underlying(borrow_amt, route_idx) * 0.999)
+    _n1 = leverage_zaps[collateral_token].calculate_debt_n1(
+        _collateral_amt, borrow_amt, N, route_idx
+    )
+    min_recv = int(
+        leverage_zaps[collateral_token].get_collateral_underlying(borrow_amt, route_idx)
+        * 0.999
+    )
 
     value = _collateral_amt if collateral_token == "WETH" else 0
     controllers[collateral_token].create_loan_extended(
@@ -48,11 +62,17 @@ def test_deleverage(
     collateral_before, _, __, ___ = controllers[collateral_token].user_state(user)
     user_collateral_before = collaterals[collateral_token].balanceOf(user)
 
-    repay_collateral_amount = min(int(collateral_before * repay_frac), collateral_before)
-    repay_debt_amount = deleverage_zaps[collateral_token].get_stablecoins(repay_collateral_amount, route_idx)
+    repay_collateral_amount = min(
+        int(collateral_before * repay_frac), collateral_before
+    )
+    repay_debt_amount = deleverage_zaps[collateral_token].get_stablecoins(
+        repay_collateral_amount, route_idx
+    )
     _n1 = 0
     if repay_debt_amount < borrow_amt:
-        _n1 = deleverage_zaps[collateral_token].calculate_debt_n1(repay_collateral_amount, route_idx, user)
+        _n1 = deleverage_zaps[collateral_token].calculate_debt_n1(
+            repay_collateral_amount, route_idx, user
+        )
 
     controllers[collateral_token].repay_extended(
         deleverage_zaps[collateral_token].address,
@@ -73,8 +93,15 @@ def test_deleverage(
     else:
         assert collateral == 0
         assert debt == 0
-        assert abs((repay_debt_amount - borrow_amt) - stablecoin_token.balanceOf(user)) / stablecoin_token.balanceOf(user) < 1e-4
-        assert collaterals[collateral_token].balanceOf(user) - user_collateral_before == collateral_before - repay_collateral_amount
+        assert (
+            abs((repay_debt_amount - borrow_amt) - stablecoin_token.balanceOf(user))
+            / stablecoin_token.balanceOf(user)
+            < 1e-4
+        )
+        assert (
+            collaterals[collateral_token].balanceOf(user) - user_collateral_before
+            == collateral_before - repay_collateral_amount
+        )
 
 
 @given(
@@ -82,32 +109,45 @@ def test_deleverage(
     repay_frac=st.floats(min_value=0.1, max_value=1),
 )
 # sfrxETH is in sunset mode, can't borrow max
-@pytest.mark.parametrize("collateral_token", ["wstETH", "WBTC", "WETH", "sfrxETH2", "tBTC"])
+@pytest.mark.parametrize(
+    "collateral_token", ["wstETH", "WBTC", "WETH", "sfrxETH2", "tBTC"]
+)
 @pytest.mark.parametrize("route_idx", [0, 1, 2, 3, 4])
 def test_deleverage_underwater(
-        collaterals,
-        controllers,
-        llammas,
-        leverage_zaps,
-        deleverage_zaps,
-        user,
-        trader,
-        stablecoin_token,
-        collateral_token,
-        collateral_amt,
-        repay_frac,
-        route_idx,
+    collaterals,
+    controllers,
+    llammas,
+    leverage_zaps,
+    deleverage_zaps,
+    user,
+    trader,
+    stablecoin_token,
+    collateral_token,
+    collateral_amt,
+    repay_frac,
+    route_idx,
 ):
     # 1. Create leveraged position
 
     N = 10
     decimals = collaterals[collateral_token].decimals()
-    balance = user.balance if collateral_token == "WETH" else collaterals[collateral_token].balanceOf(user)
+    balance = (
+        user.balance
+        if collateral_token == "WETH"
+        else collaterals[collateral_token].balanceOf(user)
+    )
     _collateral_amt = min(int(collateral_amt * 10**decimals), balance)
-    max_borrowable, _ = leverage_zaps[collateral_token].max_borrowable_and_collateral(_collateral_amt, N, route_idx)
+    max_borrowable, _ = leverage_zaps[collateral_token].max_borrowable_and_collateral(
+        _collateral_amt, N, route_idx
+    )
     borrow_amt = max_borrowable
-    _n1 = leverage_zaps[collateral_token].calculate_debt_n1(_collateral_amt, borrow_amt, N, route_idx)
-    min_recv = int(leverage_zaps[collateral_token].get_collateral_underlying(borrow_amt, route_idx) * 0.999)
+    _n1 = leverage_zaps[collateral_token].calculate_debt_n1(
+        _collateral_amt, borrow_amt, N, route_idx
+    )
+    min_recv = int(
+        leverage_zaps[collateral_token].get_collateral_underlying(borrow_amt, route_idx)
+        * 0.999
+    )
 
     value = _collateral_amt if collateral_token == "WETH" else 0
     controllers[collateral_token].create_loan_extended(
@@ -132,10 +172,14 @@ def test_deleverage_underwater(
             total_collateral_to_trade += llammas[collateral_token].bands_y(i)
     total_collateral_to_trade = total_collateral_to_trade * 10**decimals // 10**18
 
-    llammas[collateral_token].exchange_dy(0, 1, total_collateral_to_trade, 10**26, sender=trader)
+    llammas[collateral_token].exchange_dy(
+        0, 1, total_collateral_to_trade, 10**26, sender=trader
+    )
 
     active_band = llammas[collateral_token].active_band()
-    collateral_before, stablecoin_before, debt_before, _N = controllers[collateral_token].user_state(user)
+    collateral_before, stablecoin_before, debt_before, _N = controllers[
+        collateral_token
+    ].user_state(user)
 
     assert active_band > n1
     assert active_band < n2
@@ -145,7 +189,9 @@ def test_deleverage_underwater(
 
     user_collateral_before = collaterals[collateral_token].balanceOf(user)
     repay_collateral_amount = int(collateral_before * repay_frac)
-    repay_debt_amount = deleverage_zaps[collateral_token].get_stablecoins(repay_collateral_amount, route_idx)
+    repay_debt_amount = deleverage_zaps[collateral_token].get_stablecoins(
+        repay_collateral_amount, route_idx
+    )
 
     if repay_debt_amount + stablecoin_before > debt_before:  # Full repayment
         controllers[collateral_token].repay_extended(
@@ -154,11 +200,23 @@ def test_deleverage_underwater(
             sender=user,
         )
 
-        collateral, stablecoin, debt, _N = controllers[collateral_token].user_state(user)
+        collateral, stablecoin, debt, _N = controllers[collateral_token].user_state(
+            user
+        )
         assert collateral == 0
         assert debt == 0
-        assert abs((repay_debt_amount + stablecoin_before - borrow_amt) - stablecoin_token.balanceOf(user)) / stablecoin_token.balanceOf(user) < 3e-4
-        assert collaterals[collateral_token].balanceOf(user) - user_collateral_before == collateral_before - repay_collateral_amount
+        assert (
+            abs(
+                (repay_debt_amount + stablecoin_before - borrow_amt)
+                - stablecoin_token.balanceOf(user)
+            )
+            / stablecoin_token.balanceOf(user)
+            < 3e-4
+        )
+        assert (
+            collaterals[collateral_token].balanceOf(user) - user_collateral_before
+            == collateral_before - repay_collateral_amount
+        )
     # else:  # Partial repayment reverts
     #     with ape.reverts(""):
     #         controllers[collateral_token].repay_extended(
--- tests_forked/leverage/v3/conftest.py
+++ tests_forked/leverage/v3/conftest.py
@@ -7,23 +7,31 @@ import pytest
 @pytest.fixture(scope="module", autouse=True)
 def forked_chain():
     WEB3_PROVIDER_URL = os.getenv("WEB3_PROVIDER_URL")
-    assert WEB3_PROVIDER_URL is not None, "Provider url is not set, add WEB3_PROVIDER_URL param to env"
+    assert WEB3_PROVIDER_URL is not None, (
+        "Provider url is not set, add WEB3_PROVIDER_URL param to env"
+    )
     boa.fork(url=WEB3_PROVIDER_URL)
 
 
 @pytest.fixture(scope="module")
 def controller():
-    return boa.load_partial("contracts/Controller.vy").at("0xB5c6082d3307088C98dA8D79991501E113e6365d")
+    return boa.load_partial("contracts/Controller.vy").at(
+        "0xB5c6082d3307088C98dA8D79991501E113e6365d"
+    )
 
 
 @pytest.fixture(scope="module")
 def weth():
-    return boa.load_partial("contracts/testing/WETH.vy").at("0x82aF49447D8a07e3bd95BD0d56f35241523fBab1")
+    return boa.load_partial("contracts/testing/WETH.vy").at(
+        "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1"
+    )
 
 
 @pytest.fixture(scope="module")
 def crvusd():
-    return boa.load_partial("contracts/Stablecoin.vy").at("0x498Bf2B1e120FeD3ad3D42EA2165E9b73f99C1e5")
+    return boa.load_partial("contracts/Stablecoin.vy").at(
+        "0x498Bf2B1e120FeD3ad3D42EA2165E9b73f99C1e5"
+    )
 
 
 @pytest.fixture(scope="module")
@@ -37,7 +45,9 @@ def borrower(weth):
 
 @pytest.fixture(scope="module")
 def leverage_zap():
-    return boa.load("contracts/zaps/LeverageZap.vy", ["0xcaEC110C784c9DF37240a8Ce096D352A75922DeA"])
+    return boa.load(
+        "contracts/zaps/LeverageZap.vy", ["0xcaEC110C784c9DF37240a8Ce096D352A75922DeA"]
+    )
 
 
 @pytest.fixture(scope="module")
