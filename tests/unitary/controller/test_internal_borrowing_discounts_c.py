bdbfed6d1b08f99b23fe48309fef2b9ccd4caccf
Alberto <albicento.ac@gmail.com>
2025-12-10 19:14:26 +0100

    test: fix broken tests

--- tests/unitary/controller/test_internal_borrowing_discounts_c.py
+++ tests/unitary/controller/test_internal_borrowing_discounts_c.py
@@ -30,11 +30,14 @@ LOAN_DISCOUNT = 5 * 10**17
 
 def test_default_behavior(controller):
     controller.inject.set_borrowing_discounts(LOAN_DISCOUNT, LIQUIDATION_DISCOUNT)
-    logs = filter_logs("SetBorrowingDiscounts")
+    logs = filter_logs(controller, "SetBorrowingDiscounts")
 
     assert len(logs) == 1
     assert logs[0].loan_discount == LOAN_DISCOUNT
     assert logs[0].liquidation_discount == LIQUIDATION_DISCOUNT
+    
+    assert controller.loan_discount() == LOAN_DISCOUNT
+    assert controller.liquidation_discount() == LIQUIDATION_DISCOUNT
 
 
 def test_revert_liquidation_discount_zero(controller):
@@ -43,10 +46,10 @@ def test_revert_liquidation_discount_zero(controller):
 
 
 def test_revert_loan_discount_greater_or_equal_to_one(controller):
-    with boa.reverts(dev="loan discount >= 1"):
+    with boa.reverts(dev="loan discount >= 100%"):
         controller.inject.set_borrowing_discounts(10**18, LIQUIDATION_DISCOUNT)
 
-    with boa.reverts(dev="loan discount >= 1"):
+    with boa.reverts(dev="loan discount >= 100%"):
         controller.inject.set_borrowing_discounts(10**18 + 1, LIQUIDATION_DISCOUNT)
 
 
