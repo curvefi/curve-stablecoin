895c88174bec22f0fb83afda742c29f014ad1800
Alberto <albicento.ac@gmail.com>
2026-01-10 09:46:15 +0100

    fix: restore simplified conftest.py after merge
    The merge from master incorrectly kept the wrapper-based approach
    instead of the simplified direct-load approach from commit 61414c6.
    Updated path to curve_stablecoin/lending/blueprint_registry.vy.

--- tests/unitary/lib/blueprint_registry/conftest.py
+++ tests/unitary/lib/blueprint_registry/conftest.py
@@ -4,34 +4,7 @@ import pytest
 
 @pytest.fixture()
 def blueprint_registry_deployer():
-    return boa.loads_partial("""
-from curve_stablecoin.lending import blueprint_registry
-
-initializes: blueprint_registry
-
-@deploy
-def __init__(_allowed_ids: DynArray[String[4], 10]):
-    blueprint_registry.__init__(_allowed_ids)
-
-@external
-def set(_id: String[4], _address: address):
-    blueprint_registry.set(_id, _address)
-
-@external
-@view
-def get(_id: String[4]) -> address:
-    return blueprint_registry.get(_id)
-
-@external
-@view
-def in_array(_value: String[4], _array: DynArray[String[4], 10]) -> bool:
-    return blueprint_registry.in_array(_value, _array)
-
-@external
-@view
-def get_all_ids() -> DynArray[String[4], 10]:
-    return blueprint_registry.BLUEPRINT_REGISTRY_IDS
-""")
+    return boa.load_partial("curve_stablecoin/lending/blueprint_registry.vy")
 
 
 @pytest.fixture()
