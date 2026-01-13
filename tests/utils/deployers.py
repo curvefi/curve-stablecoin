e425bb91c45ee7e603911f42bfc9d9a8eb970907
Alberto <albicento.ac@gmail.com>
2026-01-13 21:04:59 +0100

    fix: use Path operator for MPOLICIES_CONTRACT_PATH

--- tests/utils/deployers.py
+++ tests/utils/deployers.py
@@ -121,7 +121,7 @@ AGG_MONETARY_POLICY3_DEPLOYER = boa.load_partial(
     compiler_args=compiler_args_default,
 )
 AGG_MONETARY_POLICY3C_DEPLOYER = boa.load_partial(
-    MPOLICIES_CONTRACT_PATH + "AggMonetaryPolicy3c.vy",
+    MPOLICIES_CONTRACT_PATH / "AggMonetaryPolicy3c.vy",
     compiler_args=compiler_args_default,
 )
 
