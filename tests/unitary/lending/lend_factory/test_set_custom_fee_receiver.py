6a96f9ba5cc935a476a4ceb3b68ca90c7ec53547
Roman Agureev <Agureev.RS@phystech.edu>
2026-03-11 18:01:54 +0300

    feat: make methods consistent

--- tests/unitary/lending/lend_factory/test_set_custom_fee_receiver.py
+++ tests/unitary/lending/lend_factory/test_set_custom_fee_receiver.py
@@ -1,5 +1,4 @@
 import boa
-from tests.utils import filter_logs
 
 
 def test_default_behavior(factory, admin, controller):
@@ -7,46 +6,25 @@ def test_default_behavior(factory, admin, controller):
     assert factory.fee_receiver(controller.address) != new_fee_receiver
 
     with boa.env.prank(admin):
-        factory.set_custom_fee_receiver(controller.address, new_fee_receiver)
-
-    logs = filter_logs(factory, "CustomSetFeeReceiver")
+        group_id = factory.add_fee_receiver_group(new_fee_receiver)
+        factory.set_fee_receiver_group(controller.address, group_id)
 
     assert factory.fee_receiver(controller.address) == new_fee_receiver
 
-    assert len(logs) == 1
-    assert logs[0].controller == controller.address
-    assert logs[0].fee_receiver == new_fee_receiver
-
-
 def test_unauthorized(factory, controller):
     non_owner = boa.env.generate_address("non_owner")
-    new_fee_receiver = boa.env.generate_address("new_fee_receiver")
     with boa.reverts("ownable: caller is not the owner"):
         with boa.env.prank(non_owner):
-            factory.set_custom_fee_receiver(controller.address, new_fee_receiver)
+            factory.set_fee_receiver_group(controller.address, 0)
 
 
-def test_revert_set_custom_fee_receiver_non_controller(factory, admin):
-    new_fee_receiver = boa.env.generate_address("new_fee_receiver")
-    non_controller = boa.env.generate_address("non_controller")
-
-    logs_before = filter_logs(factory, "CustomSetFeeReceiver")
-
-    with boa.reverts("not a controller"):
-        with boa.env.prank(admin):
-            factory.set_custom_fee_receiver(non_controller, new_fee_receiver)
-
-    logs_after = filter_logs(factory, "CustomSetFeeReceiver")
-    assert len(logs_after) == len(logs_before)
-
-
-def test_revert_set_custom_fee_receiver_vault_or_amm(factory, admin, vault, amm):
+def test_set_fee_receiver_group_assignee(factory, admin, controller):
+    first_receiver = boa.env.generate_address("first_receiver")
     new_fee_receiver = boa.env.generate_address("new_fee_receiver")
 
     with boa.env.prank(admin):
-        if vault is not None:
-            with boa.reverts("not a controller"):
-                factory.set_custom_fee_receiver(vault.address, new_fee_receiver)
+        group_id = factory.add_fee_receiver_group(first_receiver)
+        factory.set_fee_receiver_group(controller.address, group_id)
+        factory.set_fee_receiver_group_assignee(group_id, new_fee_receiver)
 
-        with boa.reverts("not a controller"):
-            factory.set_custom_fee_receiver(amm.address, new_fee_receiver)
+    assert factory.fee_receiver(controller.address) == new_fee_receiver
