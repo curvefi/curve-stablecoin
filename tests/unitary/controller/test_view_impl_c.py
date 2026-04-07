fe5d4fac3c88978857aca16793469dc1ad0f11a3
macket <macket24@proton.me>
2026-04-07 16:54:38 +0400

    chore: formatting

--- tests/unitary/controller/test_view_impl_c.py
+++ tests/unitary/controller/test_view_impl_c.py
@@ -1,2 +1,6 @@
 def test_default_behavior(controller, proto, market_type):
-    assert controller.view_impl() == proto.blueprints.lend_controller_view.address if market_type == "lending" else proto.blueprints.mint_controller_view.address
+    assert (
+        controller.view_impl() == proto.blueprints.lend_controller_view.address
+        if market_type == "lending"
+        else proto.blueprints.mint_controller_view.address
+    )
